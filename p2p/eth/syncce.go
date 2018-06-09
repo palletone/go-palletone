f54ccb9b56309bd44ad9666c21c32e9ce323296f
wangjiyou <wangjiyou@126.com>
2018-06-09 21:41:55 +0800

    delete deprecated code

--- p2p/eth/syncce.go
+++ p2p/eth/syncce.go
@@ -1,20 +1,6 @@
 package eth
 
-import (
-	//"math/rand"
-	//"sync/atomic"
-	//"time"
-
-	"github.com/palletone/go-palletone/common/log"
-	//"github.com/palletone/go-palletone/common"
-	//"github.com/palletone/go-palletone/common/event"
-	//"github.com/palletone/go-palletone/contracts/types"
-	//"github.com/palletone/go-palletone/common/log"
-	//"github.com/palletone/go-palletone/p2p/discover"
-)
-
 func (self *ProtocolManager) ceBroadcastLoop() {
-	log.Info("=========ceBroadcastLoop")
 	for {
 		select {
 		case event := <-self.ceCh:
@@ -28,19 +14,8 @@ func (self *ProtocolManager) ceBroadcastLoop() {
 }
 
 func (pm *ProtocolManager) BroadcastCe(ce string) {
-	log.Info("=========BroadcastCe:", ce)
-	//PeerCount
-	//pm.server.PeerCount()
-	counts := pm.peers.GetPeers()
-	//counts := pm.peers.Len()
-	log.Info("=========BroadcastCe have peers:", counts)
-	var index int = 0
 	peers := pm.peers.GetPeers()
-
 	for _, peer := range peers {
-		log.Info("======index:", index)
 		peer.SendConsensus(ce)
-		index++
 	}
-
 }
