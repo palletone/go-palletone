fd59bb25196a4cab41ab593635beaebcd72f5b72
Albert Gou <ggq89@qq.com>
2018-06-12 10:37:05 +0800

    实现mediator调度相关的结构体定义和方法

---
+++ consensus/dpos/mediators/mediator.go
@@ -0,0 +1,13 @@
+/**
+	@version 0.1
+	@author albert·gou
+	@time June 6, 2018
+	@brief 主要实现mediator的定义及相关功能的实现
+ */
+
+package mediators
+
+// mediator结构体 和具体的账户模型有关
+type Mediator struct {
+	Name string
+}
