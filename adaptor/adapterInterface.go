75669cde718a546229fd720c18fdc103caa73996
zxl <zxl@palliums.org>
2018-06-13 16:41:10 +0800

    add interface

--- adaptor/adapterInterface.go
+++ adaptor/adapterInterface.go
@@ -1,1 +1,17 @@
 package adaptor
+
+type adapterCryptoCurrency interface {
+	NewPrivateKey() []byte
+	GetPublicKey(key []byte) (pubkey []byte)
+	GetAddress(key []byte) (address string)
+	CreateMultiSigAddress(params string)
+	GetUnspendUTXO(params string) string
+	RawTransactionGen(params string) string
+	DecodeRawTransaction(params string) string
+	SignTransaction(params string) string
+	GetBalance(params string) string
+	GetTransactions(params string) string
+}
+
+type adapterSmartContract interface {
+}
