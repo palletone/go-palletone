db653b4cf6c0cf94bcf5a8440d160a2598f75e4c
Devin <studyzy@gmail.com>
2018-06-20 18:24:54 +0800

    Add log/config.go to git

---
+++ common/log/config.go
@@ -0,0 +1,17 @@
+package log
+
+var DefaultConfig = Config{
+	LoggerPath: "./log/out.log",
+	LoggerLvl:  "DEBUG",
+	IsDebug:    true,
+	ErrPath:    "./log/err.log",
+}
+
+// key := strings.ToLower(typ.Name()) 大写统一转小写
+type Config struct {
+	// logger
+	LoggerPath string
+	ErrPath    string
+	LoggerLvl  string
+	IsDebug    bool
+}
