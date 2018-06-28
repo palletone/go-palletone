7742ff32256e89a51097473f8b66705d3a90cefa
Yang Jay <yubing9421@qq.com>
2018-06-28 16:05:47 +0800

    Auto stash before merge of "master" and "origin/master"

--- dag/modules/transaction_test.go
+++ dag/modules/transaction_test.go
@@ -12,14 +12,14 @@ import (
 // at github.com/ethereum/tests.
 var (
 	emptyTx = NewPoolTransaction(
-		common.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d11"),
+		common.HexToAddress(" "),
 		common.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d87"),
 		big.NewInt(0),
 		nil,
 	)
 
 	rightvrsTx, _ = NewPoolTransaction(
-		common.HexToAddress("b94f5374fce5edbc8e2a8697c15331677e6ebf12"),
+		common.HexToAddress("b94f5374fce5edbc8e2a8697c15331677e612345"),
 		common.HexToAddress("b94f5374fce5edbc8e2a8697c15331677e6ebf0b"),
 		big.NewInt(10),
 		common.FromHex("5544"),
@@ -34,7 +34,7 @@ func TestTransactionEncode(t *testing.T) {
 	if err != nil {
 		t.Fatalf("encode error: %v", err)
 	}
-	should := common.FromHex("f86e0380a3000000000000000000000000000000b94f5374fce5edbc8e2a8697c15331677e6ebf0b0a825544801ca098ff921201554726367d2be8c804a7ff89ccf285ebc57dff8ae4c44b9c19ac4aa08887321be575c8095f789dd4c743dfe42c1820f9231f98a962b210e3ac2452a3")
+	should := common.FromHex("f891a3000000000000000000000000000000b94f5374fce5edbc8e2a8697c15331677e61234580a3000000000000000000000000000000b94f5374fce5edbc8e2a8697c15331677e6ebf0b0a825544801ca098ff921201554726367d2be8c804a7ff89ccf285ebc57dff8ae4c44b9c19ac4aa08887321be575c8095f789dd4c743dfe42c1820f9231f98a962b210e3ac2452a3")
 	if !bytes.Equal(txb, should) {
 		t.Errorf("encoded RLP mismatch, got %x", txb)
 	}
