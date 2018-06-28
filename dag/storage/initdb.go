adfb724eacc5b1161ef1769341f9e39875545b72
yangyu <yy@palliums.org>
2018-06-28 10:37:27 +0800

    no message

--- dag/storage/initdb.go
+++ dag/storage/initdb.go
@@ -30,6 +30,10 @@ var (
 	SPEED_PROOFS                     = []byte("sp")
 	EAENED_HEADERS_COMMISSION        = "earned_headers_commossion"
 	ALL_UNITS                        = "array_units"
+
+	// state storage
+	CONTRACT_ATTRI					= []byte("contract_")	// like contract_[contract address]_[key]
+
 )
 
 func Init(path string) *palletdb.LDBDatabase {
