f0f63fefa78ff81c341beaec9fd7c04bc9a178e8
Devin <studyzy@gmail.com>
2018-06-29 15:59:00 +0800

    Add more license header.

--- check_license.sh
+++ check_license.sh
@@ -1,6 +1,6 @@
 
 echo "Checking Go files for license headers ..." 
- missing=`find . -name "*.go" | grep -v build/ | grep -v vendor/ | grep -v statistics/ | grep -v cloudflare/ | grep -v ".pb.go" | grep -v "_test.go" | xargs grep -l -i -L "License"` 
+ missing=`find . -name "*.go" | grep -v build/ | grep -v vendor/ | grep -v statistics/ | grep -v cloudflare/ | grep -v ".pb.go" | xargs grep -l -i -L "License"` 
  if [ -z "$missing" ]; then 
  echo "All go files have license headers" 
  exit 0 
--- cmd/gptn/main.go
+++ cmd/gptn/main.go
@@ -1,3 +1,4 @@
+// Copyright 2018 PalletOne
 // Copyright 2014 The go-ethereum Authors
 // This file is part of go-ethereum.
 //
