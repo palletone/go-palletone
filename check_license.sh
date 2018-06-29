4034bc3727477bd15439031d1ac2a9d621df9014
wjy <wjy@palliums.org>
2018-06-28 23:50:16 -0700

    add check_license.sh

---
+++ check_license.sh
@@ -0,0 +1,10 @@
+
+echo "Checking Go files for license headers ..." 
+ missing=`find . -name "*.go" | grep -v build/ | grep -v vendor/ | grep -v ".pb.go" | grep -v "examples/chaincode/go/utxo/consensus/consensus.go" | xargs grep -l -L "General Public License"` 
+ if [ -z "$missing" ]; then 
+ echo "All go files have license headers" 
+ exit 0 
+ fi 
+ echo "The following files are missing license headers:" 
+ echo "$missing" 
+ exit 1 
