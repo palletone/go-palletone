e1be57588eeaeff3d2667ada35eff17f65948a2b
wangjiyou <wangjiyou@126.com>
2018-07-04 02:58:14 -0700

    modify common/types_test except TestAddressHexChecksum

--- common/address.go
+++ common/address.go
@@ -28,6 +28,7 @@ import (
 
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/palletone/go-palletone/common/hexutil"
+	"math/big"
 )
 
 const (
@@ -98,7 +99,7 @@ func IsHexAddress(s string) bool {
 func (a Address) Str() string   { return string(a[:]) }
 func (a Address) Bytes() []byte { return a[:] }
 
-//func (a Address) Big() *big.Int { return new(big.Int).SetBytes(a[:]) }
+func (a Address) Big() *big.Int { return new(big.Int).SetBytes(a[:]) }
 func (a Address) Hash() Hash  { return BytesToHash(a[:]) }
 func (a Address) Hex() string { return fmt.Sprintf("0x%x", a) }
 
