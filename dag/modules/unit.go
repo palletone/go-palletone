251188a6298202f8ce014ecc45de7477afe2321a
wangjiyou <wangjiyou@126.com>
2018-07-16 01:38:47 -0700

    modify unit.go line 1

--- dag/modules/unit.go
+++ dag/modules/unit.go
@@ -1,5 +1,4 @@
-/*
-   This file is part of go-palletone.
+/* This file is part of go-palletone.
    go-palletone is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
