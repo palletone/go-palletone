b9053e127a5b0477129964eff7d0e89370e10aef
Albert·Gou <ggq89@qq.com>
2018-07-16 19:40:29 +0800

    Optimize mediator plugin initialization and startup

--- consensus/mediatorplugin/mediatorplugin.go
+++ consensus/mediatorplugin/mediatorplugin.go
@@ -1,9 +1,13 @@
 package mediatorplugin
 
+import (
+	"github.com/palletone/go-palletone/core/accounts"
+)
+
 type MediatorPlugin struct {
 	// Enable VerifiedUnit production, even if the chain is stale.
 	// 新开启一个区块链时，必须设为true
 	ProductionEnabled bool
-	// Mediator`s address and passphrase controlled by this node
-	Mediators map[string]string
+	// Mediator`s account and passphrase controlled by this node
+	Mediators map[accounts.Account]string
 }
