9f8bee32c0b83fd4a9e9d1941e48f702649eb577
Albert·Gou <ggq89@qq.com>
2018-07-16 09:55:28 +0800

    mediator plugin initialize

--- consensus/mediatorplugin/config.go
+++ consensus/mediatorplugin/config.go
@@ -9,22 +9,30 @@ var (
 	}
 )
 
+// mediator 结构体 和具体的账户模型有关
+type Mediator struct {
+	Address string
+	Passphrase string
+}
+
 // config data for mediator plugin
 type Config struct {
 	EnableStaleProduction bool	// Enable Verified Unit production, even if the chain is stale.
 //	RequiredParticipation float32	// Percent of mediators (0-99) that must be participating in order to produce
-//	PrivateKey map[string]string	//	Tuple of [PublicKey, WIF private key]
+	Mediators []Mediator
 }
 
 // mediator plugin default config
 var DefaultConfig = Config{
 	EnableStaleProduction:	false,
-//	PrivateKey:				map[string]string{"":""},
+	Mediators: []Mediator{},
 }
 
 func SetMediatorPluginConfig(ctx *cli.Context, cfg *Config)  {
 	switch  {
 	case ctx.GlobalIsSet(StaleProductionFlag.Name):
 		cfg.EnableStaleProduction = ctx.GlobalBool(StaleProductionFlag.Name)
+	//case :
+	//
 	}
 }
--- consensus/mediatorplugin/mediatorplugin.go
+++ consensus/mediatorplugin/mediatorplugin.go
@@ -4,4 +4,6 @@ type MediatorPlugin struct {
 	// Enable VerifiedUnit production, even if the chain is stale.
 	// 新开启一个区块链时，必须设为true
 	ProductionEnabled bool
+	// Mediator`s address and passphrase controlled by this node
+	Mediators map[string]string
 }
