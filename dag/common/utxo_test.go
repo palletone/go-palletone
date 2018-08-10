6b5f61dfe585a86c4cf1d9e6e33a4bb77203412e
shuizhongmose <602554102@qq.com>
2018-08-10 15:18:01 +0800

    achive coinbase transaction and add it to createunit method

--- dag/common/utxo_test.go
+++ dag/common/utxo_test.go
@@ -60,7 +60,7 @@ func TestWalletBalance(t *testing.T) {
 
 func TestGetAccountTokens(t *testing.T) {
 	addr := common.Address{}
-	addr.SetString("P14yBqQ4ogJ3RdEFoVTdd5ucsdu3PffZZRu")
+	addr.SetString("P12EA8oRMJbAtKHbaXGy8MGgzM8AMPYxkNr")
 	tokens, err := GetAccountTokens(addr)
 	if err != nil {
 		log.Println("Get account error:", err.Error())
