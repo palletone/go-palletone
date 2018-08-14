80bdda85c8c3da00cf0daece37adaa503c77cfa4
wangjiyou <wangjiyou@126.com>
2018-08-14 04:08:35 -0700

    modify DbPath default path

--- dag/dagconfig/config.go
+++ dag/dagconfig/config.go
@@ -18,12 +18,20 @@
 
 package dagconfig
 
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+)
+
 var (
 	SConfig Sconfig
 )
 
 var DefaultConfig = Config{
-	DbPath: "./db/leveldb",
+	DbPath: DefaultDataDir(),
+	//DbPath: "./db/leveldb",
 	// DbPath: "../../cmd/gptn/leveldb",
 	DbName: "palletone",
 
@@ -59,4 +67,32 @@ type Config struct {
 
 type Sconfig struct {
 	Blight bool
+}
+
+// DefaultDataDir is the default data directory to use for the databases and other
+// persistence requirements.
+func DefaultDataDir() string {
+	// Try to place the data folder in the user's home dir
+	home := homeDir()
+	if home != "" {
+		if runtime.GOOS == "darwin" {
+			return filepath.Join(home, "Library", "PalletOne")
+		} else if runtime.GOOS == "windows" {
+			return filepath.Join(home, "AppData", "Roaming", "PalletOne")
+		} else {
+			return filepath.Join(home, ".palletone")
+		}
+	}
+	// As we cannot guess a stable location, return empty and handle later
+	return ""
+}
+
+func homeDir() string {
+	if home := os.Getenv("HOME"); home != "" {
+		return home
+	}
+	if usr, err := user.Current(); err == nil {
+		return usr.HomeDir
+	}
+	return ""
 }
