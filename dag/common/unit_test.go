5298f08d777c6c15380a7257c9973117c279565f
shuizhongmose <602554102@qq.com>
2018-08-21 12:02:59 +0800

    add parameter time for createunit method, and add time bytes to coinbase transaction.

--- dag/common/unit_test.go
+++ dag/common/unit_test.go
@@ -195,7 +195,7 @@ func TestCreateUnit(t *testing.T) {
 	addr := common.Address{} // minner addr
 	addr.SetString("P1FYoQg1QHxAuBEgDy7c5XDWh3GLzLTmrNM")
 	//units, err := CreateUnit(&addr, time.Now())
-	units, err := CreateUnit(&addr, nil, nil)
+	units, err := CreateUnit(&addr, nil, nil, time.Now())
 	if err != nil {
 		log.Println("create unit error:", err)
 	} else {
