b14ff8c524fac80638f6249c460916fa2484c77d
guolihua <guolihua@163.com>
2018-08-27 19:50:41 -0700

    change endorser interface

--- core/vmContractPub/protos/peer/peer.pb.go
+++ core/vmContractPub/protos/peer/peer.pb.go
@@ -3,8 +3,9 @@ package peer
 import (
 	"golang.org/x/net/context"
 	"time"
+	unit "github.com/palletone/go-palletone/dag/modules"
 )
 
 type EndorserServer interface {
-	ProcessProposal(context.Context, *SignedProposal, *Proposal, string, *ChaincodeID, time.Duration) (*ProposalResponse, *ContractInvokePayload, error)
+	ProcessProposal([]byte, context.Context, *SignedProposal, *Proposal, string, *ChaincodeID, time.Duration) (*ProposalResponse, *unit.ContractInvokePayload, error)
 }
