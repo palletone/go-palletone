69b641d8f90113fb77174ec9ef32db32039a14ed
wuzhiyuan <wzhyuan0621@126.com>
2018-09-06 18:11:52 +0800

    update decode rlp payload method

--- internal/ptnapi/api.go
+++ internal/ptnapi/api.go
@@ -1549,16 +1549,8 @@ func SignRawTransaction(icmd interface{}) (interface{}, error) {
 		if msg.App != modules.APP_PAYMENT {
 			continue
 		} else {
-                         payload := msg.Payload
-		         payment := payload.(*modules.PaymentPayload)
-                         fmt.Printf("-1554 is payment is --------%+v\n",payment)
-			 //if !ok {
-				fmt.Println("--------1556    not ok  Get Payment payload error:")
-			 //} //else {
-			 //	fmt.Println("Payment payload:", payload)
-			 //}
+                         payload := msg.Payload.(*modules.PaymentPayload)
 			 for i, txIn := range msg.Payload.(*modules.PaymentPayload).Input {
-                                fmt.Println("-----1561-----txIn-----------------",txIn)
 				prevOutScript, _ := inputpoints[txIn.PreviousOutPoint]
 
 				// Set up our callbacks that we pass to txscript so it can
@@ -1585,15 +1577,13 @@ func SignRawTransaction(icmd interface{}) (interface{}, error) {
 				// SigHashSingle inputs can only be signe   d if there's a
 				// corresponding output. However this could be already signed,
 				// so we always verify the output.
-                                fmt.Println("--------1588-----1588------")
 				if (hashType&txscript.SigHashSingle) !=
-					txscript.SigHashSingle || i < len(msg.Payload.(*modules.PaymentPayload).Output) {
+					txscript.SigHashSingle || i < len(payload.Output) {
 					script, err := txscript.SignTxOutput(params,
-						payment, i, prevOutScript, hashType, geekey,
+						payload, i, prevOutScript, hashType, geekey,
 						getScript, txIn.SignatureScript)
 					// Failure to sign isn't an error, it just means that
 					// the tx isn't complete.
-                                        fmt.Println("------------1596-----------------------")
 					if err != nil {
                                                 fmt.Println("err is not nil")
 						signErrors = append(signErrors, SignatureError{
@@ -1603,12 +1593,10 @@ func SignRawTransaction(icmd interface{}) (interface{}, error) {
 						continue
 					}
 					txIn.SignatureScript = script
-                                        fmt.Println("------1603--------",script)
 				}
-                                fmt.Println("-----1606----------")
 				// Either it was already signed or we just signed it.
 				// Find out if it is completely satisfied or still needs more.
-				vm, err := txscript.NewEngine(prevOutScript, payment, i,
+				vm, err := txscript.NewEngine(prevOutScript, payload, i,
 					txscript.StandardVerifyFlags, nil, nil, 0)
 				if err == nil {
 					err = vm.Execute()
