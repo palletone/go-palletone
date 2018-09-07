842d336be2dfea442bc4a71dc5a2f5dc678d7322
zhichunqi <1558763837@qq.com>
2018-09-07 14:13:10 +0800

    delete units_make.go
    delete units_make.go and move codes in units_make.go to helper_test.go

--- ptn/helper_test.go
+++ ptn/helper_test.go
@@ -42,6 +42,9 @@ import (
 	"github.com/palletone/go-palletone/common/ptndb"
 	"github.com/palletone/go-palletone/consensus/mediatorplugin"
 	common2 "github.com/palletone/go-palletone/dag/common"
+	"time"
+	"github.com/palletone/go-palletone/dag/storage"
+	"github.com/palletone/go-palletone/dag"
 )
 
 //var (
@@ -227,3 +230,144 @@ func (p *testPeer) handshake(t *testing.T, td uint64, head common.Hash, genesis 
 func (p *testPeer) close() {
 	p.app.Close()
 }
+
+func MakeDags(Memdb ptndb.Database, unitAccount int) (*dag.Dag, error) {
+	dag, _ := dag.NewDagForTest(Memdb)
+	header := NewHeader([]common.Hash{}, []modules.IDType16{modules.PTNCOIN}, []byte{})
+	header.Number.AssetID = modules.PTNCOIN
+	header.Number.IsMain = true
+	header.Number.Index = 0
+	header.Authors = &modules.Authentifier{"", []byte{}, []byte{}, []byte{}}
+	header.Witness = []*modules.Authentifier{&modules.Authentifier{"", []byte{}, []byte{}, []byte{}}}
+	tx, _ := NewCoinbaseTransaction()
+	txs := modules.Transactions{tx}
+	genesisUnit := NewUnit(header, txs)
+	err := SaveGenesis(dag.Db, genesisUnit)
+	if err != nil {
+		log.Println("SaveGenesis, err", err)
+		return nil, err
+	}
+	newDag(dag.Db, genesisUnit, unitAccount)
+	return dag, nil
+}
+func newDag(memdb ptndb.Database, gunit *modules.Unit, number int) (modules.Units, error) {
+	units := make(modules.Units, number)
+	par := gunit
+	for i := 0; i < number; i++ {
+		header := NewHeader([]common.Hash{par.UnitHash}, []modules.IDType16{modules.PTNCOIN}, []byte{})
+		header.Number.AssetID = par.UnitHeader.Number.AssetID
+		header.Number.IsMain = par.UnitHeader.Number.IsMain
+		header.Number.Index = par.UnitHeader.Number.Index + 1
+		header.Authors = &modules.Authentifier{"", []byte{}, []byte{}, []byte{}}
+		header.Witness = []*modules.Authentifier{&modules.Authentifier{"", []byte{}, []byte{}, []byte{}}}
+		tx, _ := NewCoinbaseTransaction()
+		txs := modules.Transactions{tx}
+		unit := NewUnit(header, txs)
+		err := SaveUnit(memdb, unit, true)
+		if err != nil {
+			log.Println("save genesis error", err)
+			return nil, err
+		}
+		units[i] = unit
+		par = unit
+	}
+	return units, nil
+}
+func SaveGenesis(db ptndb.Database, unit *modules.Unit) error {
+	if unit.NumberU64() != 0 {
+		return fmt.Errorf("can't commit genesis unit with number > 0")
+	}
+	err := SaveUnit(db, unit, true)
+	if err != nil {
+		log.Println("SaveGenesis==", err)
+		return err
+	}
+	return nil
+}
+
+func SaveUnit(db ptndb.Database, unit *modules.Unit, isGenesis bool) error {
+	if unit.UnitSize == 0 || unit.Size() == 0 {
+		log.Println("Unit is null")
+		return fmt.Errorf("Unit is null")
+	}
+	if unit.UnitSize != unit.Size() {
+		log.Println("Validate size", "error", "Size is invalid")
+		return modules.ErrUnit(-1)
+	}
+	_, isSuccess, err := common2.ValidateTransactions(db, &unit.Txs, isGenesis)
+	if isSuccess != true {
+		fmt.Errorf("Validate unit(%s) transactions failed: %v", unit.UnitHash.String(), err)
+		return fmt.Errorf("Validate unit(%s) transactions failed: %v", unit.UnitHash.String(), err)
+	}
+	// step4. save unit header
+	// key is like "[HEADER_PREFIX][chain index number]_[chain index]_[unit hash]"
+	if err := storage.SaveHeader(db, unit.UnitHash, unit.UnitHeader); err != nil {
+		log.Println("SaveHeader:", "error", err.Error())
+		return modules.ErrUnit(-3)
+	}
+	// step5. save unit hash and chain index relation
+	// key is like "[UNIT_HASH_NUMBER][unit_hash]"
+	if err := storage.SaveNumberByHash(db, unit.UnitHash, unit.UnitHeader.Number); err != nil {
+		log.Println("SaveHashNumber:", "error", err.Error())
+		return fmt.Errorf("Save unit hash and number error")
+	}
+	if err := storage.SaveHashByNumber(db, unit.UnitHash, unit.UnitHeader.Number); err != nil {
+		log.Println("SaveNumberByHash:", "error", err.Error())
+		return fmt.Errorf("Save unit hash and number error")
+	}
+	if err := storage.SaveTxLookupEntry(db, unit); err != nil {
+		return err
+	}
+	if err := storage.SaveTxLookupEntry(db, unit); err != nil {
+		return err
+	}
+	if err := saveHashByIndex(db, unit.UnitHash, unit.UnitHeader.Number.Index); err != nil {
+		return err
+	}
+	// update state
+	storage.PutCanonicalHash(db, unit.UnitHash, unit.NumberU64())
+	storage.PutHeadHeaderHash(db, unit.UnitHash)
+	storage.PutHeadUnitHash(db, unit.UnitHash)
+	storage.PutHeadFastUnitHash(db, unit.UnitHash)
+	// todo send message to transaction pool to delete unit's transactions
+	return nil
+}
+func NewUnit(header *modules.Header, txs modules.Transactions) *modules.Unit {
+	u := &modules.Unit{
+		UnitHeader: header,
+		Txs:        txs,
+	}
+	u.ReceivedAt = time.Now()
+	u.UnitSize = u.Size()
+	u.UnitHash = u.Hash()
+	return u
+}
+func NewHeader(parents []common.Hash, asset []modules.IDType16, extra []byte) *modules.Header {
+	hashs := make([]common.Hash, 0)
+	hashs = append(hashs, parents...) // 切片指针传递的问题，这里得再review一下。
+	var b []byte
+	return &modules.Header{ParentsHash: hashs, AssetIDs: asset, Extra: append(b, extra...), Creationdate: time.Now().Unix()}
+}
+func NewCoinbaseTransaction() (*modules.Transaction, error) {
+	input := &modules.Input{}
+	output := &modules.Output{}
+	payload := modules.PaymentPayload{
+		Input:  []*modules.Input{input},
+		Output: []*modules.Output{output},
+	}
+	msg := modules.Message{
+		App:     modules.APP_PAYMENT,
+		Payload: payload,
+	}
+	var coinbase modules.Transaction
+	coinbase.TxMessages = append(coinbase.TxMessages, &msg)
+	coinbase.TxHash = coinbase.Hash()
+	return &coinbase, nil
+}
+
+func saveHashByIndex(db ptndb.Database, hash common.Hash, index uint64) error {
+	key := fmt.Sprintf("%s%v_", storage.HEADER_PREFIX, index)
+	err := db.Put([]byte(key), hash.Bytes())
+	return err
+}
+
--- ptn/sync_test.go
+++ ptn/sync_test.go
@@ -1,22 +1,32 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+//// Copyright 2015 The go-ethereum Authors
+//// This file is part of the go-ethereum library.
+////
+//// The go-ethereum library is free software: you can redistribute it and/or modify
+//// it under the terms of the GNU Lesser General Public License as published by
+//// the Free Software Foundation, either version 3 of the License, or
+//// (at your option) any later version.
+////
+//// The go-ethereum library is distributed in the hope that it will be useful,
+//// but WITHOUT ANY WARRANTY; without even the implied warranty of
+//// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//// GNU Lesser General Public License for more details.
+////
+//// You should have received a copy of the GNU Lesser General Public License
+//// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package ptn
-
-// Tests that fast sync gets disabled as soon as a real block is successfully imported into the blockchain.
+//
+//import (
+//	"testing"
+//	"sync/atomic"
+//	"time"
+//	"github.com/palletone/go-palletone/dag/modules"
+//	"github.com/palletone/go-palletone/ptn/downloader"
+//	"github.com/palletone/go-palletone/common/p2p"
+//	"github.com/palletone/go-palletone/common/p2p/discover"
+//)
+//
+//// Tests that fast sync gets disabled as soon as a real block is successfully imported into the blockchain.
 //func TestFastSyncDisabling(t *testing.T) {
 //	// Create a pristine protocol manager, check that fast sync is left enabled
 //	pmEmpty, _ := newTestProtocolManagerMust(t, downloader.FastSync, 0, nil)
@@ -43,4 +53,4 @@ package ptn
 //		t.Fatalf("fast sync not disabled after successful synchronisation")
 //	}
 //}
-
+//
