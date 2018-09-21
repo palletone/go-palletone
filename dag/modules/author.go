93a28f726f1ba2c9daed8172fe7920165e612fb2
Yang Jay <yubing9421@qq.com>
2018-09-21 16:08:17 +0800

    modify dag module structure , and fixed txpool

--- dag/modules/author.go
+++ dag/modules/author.go
@@ -17,9 +17,3 @@
  */
 
 package modules
-
-type UnitAuthors struct {
-	Unit           string `json:"unit"`
-	BestParentUnit string `json:"best_parent_unit"`
-	WitnessedLevel int64  `json:"witnessed_level"`
-}
