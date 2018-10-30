c8cb49dd2c20cb6b83ba4804aa7ffc9eed2bb704
Albert·Gou <ggq89@qq.com>
2018-10-30 17:03:50 +0800

    refactor mediator Command

--- cmd/gptn/misccmd.go
+++ cmd/gptn/misccmd.go
@@ -20,19 +20,13 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
-	"time"
 
-	"github.com/dedis/kyber/pairing/bn256"
 	"github.com/palletone/go-palletone/cmd/utils"
-	"github.com/palletone/go-palletone/common/p2p/discover"
 	"github.com/palletone/go-palletone/configure"
-	mp "github.com/palletone/go-palletone/consensus/mediatorplugin"
-	"github.com/palletone/go-palletone/core"
 	"github.com/palletone/go-palletone/ptn"
 	"gopkg.in/urfave/cli.v1"
 )
@@ -80,53 +74,6 @@ The output of this command is supposed to be machine-readable.
 		Usage:     "Display license information",
 		ArgsUsage: " ",
 		Category:  "MISCELLANEOUS COMMANDS",
-	}
-
-	// append by Albert·Gou
-	createInitDKSCommand = cli.Command{
-		Action:    utils.MigrateFlags(createInitDKS),
-		Name:      "initdks",
-		Usage:     "Generate the initial distributed key share",
-		ArgsUsage: "",
-		Category:  "MEDIATOR COMMANDS",
-		Description: `
-The output of this command will be used to initialize the DistKeyGenerator.
-`,
-	}
-
-	// append by Albert·Gou
-	nodeInfoCommand = cli.Command{
-		Action:    utils.MigrateFlags(showNodeInfo),
-		Name:      "nodeInfo",
-		Usage:     "get info of current node",
-		ArgsUsage: "",
-		Category:  "MEDIATOR COMMANDS",
-		Description: `
-The output of this command will be used to set the genesis json file.
-`,
-	}
-
-	// append by Albert·Gou
-	timestampCommand = cli.Command{
-		Action:    utils.MigrateFlags(getTimestamp),
-		Name:      "timestamp",
-		Usage:     "get the timestamp of the Unix epoch at the specified time",
-		ArgsUsage: "<specified time>",
-		Flags: []cli.Flag{
-			timeStringFlag,
-		},
-		Category: "MEDIATOR COMMANDS",
-		Description: `
-The format of the specified time should be like "2006-01-02 15:04:05", 
-and If not specified, displays the timestamp of 
-the time when the current command is running.
-`,
-	}
-
-	timeStringFlag = cli.StringFlag{
-		Name:  "timeString",
-		Usage: "time formatted as \"2006-01-02 15:04:05\"",
-		Value: "",
 	}
 )
 
@@ -187,84 +134,5 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with gptn. If not, see <http://www.gnu.org/licenses/>.`)
-	return nil
-}
-
-// author Albert·Gou
-func newInitDKS() (secStr, pubStr string) {
-	suite := bn256.NewSuiteG2()
-	sec, pub := mp.GenInitPair(suite)
-
-	secStr = core.ScalarToStr(sec)
-	pubStr = core.PointToStr(pub)
-
-	return
-}
-
-// author Albert·Gou
-func createInitDKS(ctx *cli.Context) error {
-	secStr, pubStr := newInitDKS()
-
-	fmt.Println("Generate a initial distributed key share:")
-	fmt.Println("{")
-	fmt.Println("\tprivate key: ", secStr)
-	fmt.Println("\tpublic key: ", pubStr)
-	fmt.Println("}")
-
-	return nil
-}
-
-// author Albert·Gou
-func getNodeInfo(ctx *cli.Context) (string, error) {
-	stack := makeFullNode(ctx)
-	privateKey := stack.Config().NodeKey()
-	listenAddr := stack.ListenAddr()
-
-	listener, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		return "", err
-		utils.Fatalf("Invalid listen address : %v", err)
-	}
-	realaddr := listener.Addr().(*net.TCPAddr)
-
-	node := discover.NewNode(
-		discover.PubkeyID(&privateKey.PublicKey),
-		realaddr.IP,
-		uint16(realaddr.Port),
-		uint16(realaddr.Port))
-
-	return node.String(), nil
-}
-
-// author Albert·Gou
-func showNodeInfo(ctx *cli.Context) error {
-	nodeStr, err := getNodeInfo(ctx)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(nodeStr)
-
-	return nil
-}
-
-// author Albert·Gou
-func getTimestamp(ctx *cli.Context) error {
-	var timeUnix time.Time
-	var err error
-
-	timeStr := ctx.Args().First()
-	if len(timeStr) == 0 {
-		timeUnix = time.Now()
-	} else {
-		timeUnix, err = time.Parse("2006-01-02 15:04:05", timeStr)
-		if err != nil {
-			fmt.Println("Please enter the time in the following format: \"2006-01-02 15:04:05\"")
-			return nil
-		}
-	}
-
-	fmt.Println(timeUnix.Unix())
-
 	return nil
 }
