f80ee165628c680519904f87b3d5a511e17a6dd3
Albert·Gou <ggq89@qq.com>
2018-11-30 15:52:28 +0800

    Adjust the calling order of the ApplyUnit method

--- dag/dag.go
+++ dag/dag.go
@@ -258,10 +258,7 @@ func (d *Dag) InsertDag(units modules.Units, txpool txspool.ITxPool) (int, error
 	}(time.Now())
 	count := int(0)
 	for i, u := range units {
-		// append by albert·gou
-		time := time.Unix(u.Timestamp(), 0)
-		log.Info(fmt.Sprint("Got unit # ", u.NumberU64(), " timestamp: ", time.Format("2006-01-02 15:04:05")))
-		d.ApplyUnit(u)
+		// todo 此处为判断 第 0 个 unit 的父unit是否已存入版节点
 
 		// all units must be continuous
 		if i > 0 && units[i].UnitHeader.Number.Index == units[i-1].UnitHeader.Number.Index+1 {
@@ -278,6 +275,12 @@ func (d *Dag) InsertDag(units modules.Units, txpool txspool.ITxPool) (int, error
 				units[i-1].UnitHeader.Number.Index, units[i-1].UnitHash,
 				units[i].UnitHeader.Number.Index, units[i].UnitHash)
 		}
+
+		// append by albert·gou
+		time := time.Unix(u.Timestamp(), 0)
+		log.Info(fmt.Sprint("Got unit # ", u.NumberU64(), " timestamp: ", time.Format("2006-01-02 15:04:05")))
+		d.ApplyUnit(u)
+
 		// todo 应当和本地生产的unit统一接口，而不是直接存储
 		// modified by albert·gou
 		//if err := d.unitRep.SaveUnit(u, false); err != nil {
