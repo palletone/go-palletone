980ce91dc072339e36c8303804097317b6affbcb
wuzhiyuan <wzhyuan0621@126.com>
2019-01-09 20:15:45 +0800

    Add CreateProofTransaction

--- internal/ptnapi/walletapi.go
+++ internal/ptnapi/walletapi.go
@@ -241,6 +241,170 @@ func (s *PublicWalletAPI) SendRawTransaction(ctx context.Context, params string)
 	return submitTransaction(ctx, s.b, mtx)
 }
 
+
+func (s *PublicWalletAPI) CreateProofTransaction(ctx context.Context, from string, to string, amount, fee decimal.Decimal) (string, error) {
+
+	//realNet := &chaincfg.MainNetParams
+	var LockTime int64
+	LockTime = 0
+
+	amounts := []ptnjson.AddressAmt{}
+	if to == "" {
+		return "", fmt.Errorf("amounts is empty")
+	}
+
+	amounts = append(amounts, ptnjson.AddressAmt{to, amount})
+
+	utxoJsons, err := s.b.GetAddrUtxos(from)
+	if err != nil {
+		return "", err
+	}
+	utxos := core.Utxos{}
+	ptn := modules.CoreAsset.String()
+	for _, json := range utxoJsons {
+		//utxos = append(utxos, &json)
+		if json.Asset == ptn {
+			utxos = append(utxos, &ptnjson.UtxoJson{TxHash: json.TxHash, MessageIndex: json.MessageIndex, OutIndex: json.OutIndex, Amount: json.Amount, Asset: json.Asset, PkScriptHex: json.PkScriptHex, PkScriptString: json.PkScriptString, LockTime: json.LockTime})
+		}
+	}
+	if !fee.IsPositive() {
+		return "", fmt.Errorf("fee is ZERO ")
+	}
+	daoAmount := ptnjson.Ptn2Dao(amount.Add(fee))
+
+	taken_utxo, change, err := core.Select_utxo_Greedy(utxos, daoAmount)
+	if err != nil {
+		return "", fmt.Errorf("Select utxo err")
+	}
+
+	var inputs []ptnjson.TransactionInput
+	var input ptnjson.TransactionInput
+	for _, u := range taken_utxo {
+		utxo := u.(*ptnjson.UtxoJson)
+		input.Txid = utxo.TxHash
+		input.MessageIndex = utxo.MessageIndex
+		input.Vout = utxo.OutIndex
+		inputs = append(inputs, input)
+	}
+
+	if change > 0 {
+		amounts = append(amounts, ptnjson.AddressAmt{from, ptnjson.Dao2Ptn(change)})
+	}
+
+	arg := ptnjson.NewCreateRawTransactionCmd(inputs, amounts, &LockTime)
+	result, _ := WalletCreateProofTransaction(arg)
+	//fmt.Println(result)
+	return result, nil
+}
+func WalletCreateProofTransaction( /*s *rpcServer*/ c *ptnjson.CreateRawTransactionCmd) (string, error) {
+
+	// Validate the locktime, if given.
+	if c.LockTime != nil &&
+		(*c.LockTime < 0 || *c.LockTime > int64(MaxTxInSequenceNum)) {
+		return "", &ptnjson.RPCError{
+			Code:    ptnjson.ErrRPCInvalidParameter,
+			Message: "Locktime out of range",
+		}
+	}
+	// Add all transaction inputs to a new transaction after performing
+	// some validity checks.
+	//先构造PaymentPayload结构，再组装成Transaction结构
+	pload := new(modules.PaymentPayload)
+	var inputjson []walletjson.InputJson
+	for _, input := range c.Inputs {
+		txHash, err := common.NewHashFromStr(input.Txid)
+		if err != nil {
+			return "", rpcDecodeHexError(input.Txid)
+		}
+		inputjson = append(inputjson, walletjson.InputJson{TxHash: input.Txid, MessageIndex: input.MessageIndex, OutIndex: input.Vout, HashForSign: "", Signature: ""})
+		prevOut := modules.NewOutPoint(txHash, input.MessageIndex, input.Vout)
+		txInput := modules.NewTxIn(prevOut, []byte{})
+		pload.AddTxIn(txInput)
+	}
+	var OutputJson []walletjson.OutputJson
+	// Add all transaction outputs to the transaction after performing
+	//	// some validity checks.
+	//	//only support mainnet
+	//	var params *chaincfg.Params
+	for _, addramt := range c.Amounts {
+		encodedAddr := addramt.Address
+		ptnAmt := addramt.Amount
+		amount := ptnjson.Ptn2Dao(ptnAmt)
+		//		// Ensure amount is in the valid range for monetary amounts.
+		if amount <= 0 /*|| amount > ptnjson.MaxDao*/ {
+			return "", &ptnjson.RPCError{
+				Code:    ptnjson.ErrRPCType,
+				Message: "Invalid amount",
+			}
+		}
+		addr, err := common.StringToAddress(encodedAddr)
+		if err != nil {
+			return "", &ptnjson.RPCError{
+				Code:    ptnjson.ErrRPCInvalidAddressOrKey,
+				Message: "Invalid address or key",
+			}
+		}
+		switch addr.GetType() {
+		case common.PublicKeyHash:
+		case common.ScriptHash:
+		case common.ContractHash:
+			//case *ptnjson.AddressPubKeyHash:
+			//case *ptnjson.AddressScriptHash:
+		default:
+			return "", &ptnjson.RPCError{
+				Code:    ptnjson.ErrRPCInvalidAddressOrKey,
+				Message: "Invalid address or key",
+			}
+		}
+		// Create a new script which pays to the provided address.
+		pkScript := tokenengine.GenerateLockScript(addr)
+		// Convert the amount to satoshi.
+		dao := ptnjson.Ptn2Dao(ptnAmt)
+		if err != nil {
+			context := "Failed to convert amount"
+			return "", internalRPCError(err.Error(), context)
+		}
+		asset := modules.NewPTNAsset()
+		txOut := modules.NewTxOut(uint64(dao), pkScript, asset)
+		pload.AddTxOut(txOut)
+		OutputJson = append(OutputJson, walletjson.OutputJson{Amount: uint64(dao), Asset: asset.String(), ToAddress: addr.String()})
+	}
+	//	// Set the Locktime, if given.
+	if c.LockTime != nil {
+		pload.LockTime = uint32(*c.LockTime)
+	}
+	//	// Return the serialized and hex-encoded transaction.  Note that this
+	//	// is intentionally not directly returning because the first return
+	//	// value is a string and it would result in returning an empty string to
+	//	// the client instead of nothing (nil) in the case of an error.
+
+	mtx := &modules.Transaction{
+		TxMessages: make([]*modules.Message, 0),
+	}
+	mtx.TxMessages = append(mtx.TxMessages, modules.NewMessage(modules.APP_PAYMENT, pload))
+	//mtx.TxHash = mtx.Hash()
+	// sign mtx
+	for index, input := range inputjson {
+		hashforsign, err := tokenengine.CalcSignatureHash(mtx, int(input.MessageIndex), int(input.OutIndex), nil)
+		if err != nil {
+			return "", err
+		}
+		sh := common.BytesToHash(hashforsign)
+		inputjson[index].HashForSign = sh.String()
+	}
+	PaymentJson := walletjson.PaymentJson{}
+	PaymentJson.Inputs = inputjson
+	PaymentJson.Outputs = OutputJson
+	txjson := walletjson.TxJson{}
+	txjson.Payload = append(txjson.Payload, PaymentJson)
+	bytetxjson, err := json.Marshal(txjson)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytetxjson), nil
+}
+
 func (s *PublicWalletAPI) GetAddrUtxos(ctx context.Context, addr string) (string, error) {
 
 	items, err := s.b.GetAddrUtxos(addr)
