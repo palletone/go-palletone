f4a1acb491187697acd46d67047ed30c6c8f78e5
Albert·Gou <ggq89@qq.com>
2019-01-22 16:23:54 +0800

    fix bug no create tokenHolder in modifyjson.sh

--- cmd/deployment/modifyjson.sh
+++ cmd/deployment/modifyjson.sh
@@ -26,14 +26,33 @@ else
 fi
 !
 
-add=`cat $filename | jq ".initialMediatorCandidates[$4-1] |= . + {\"account\": \"$1\", \"initPubKey\": \"$2\", \"node\": \"$3\"}"`
+index=$[ $4 - 1 ]
+
+add=`cat $filename | jq ".initialMediatorCandidates[$index] |= . + {\"account\": \"$1\", \"initPubKey\": \"$2\", \"node\": \"$3\"}"`
+
+if [ $index -eq 0 ] ; then
+
+    createaccount=`./createaccount.sh`
+    tempinfo=`echo $createaccount | sed -n '$p'| awk '{print $NF}'`
+    accountlength=35
+    accounttemp=${tempinfo:0:$accountlength}
+    account=`echo ${accounttemp//^M/}`
+
+    add=`echo $add |
+       jq "to_entries |
+       map(if .key == \"tokenHolder\"
+          then . + {\"value\":\"$account\"}
+          else .
+          end
+         ) |
+      from_entries"`
+
+fi
 
     rm $filename
     echo $add >> temp.json
     jq -r . temp.json >> $filename
     rm temp.json
 }
-
-
 
 
