23cfe64316dca7b2fb8542001fec772d0fdf41a6
zhichunqi <1558763837@qq.com>
2019-01-23 04:11:35 -0800

    Add common dir to generate the common  base image

---
+++ vm/baseimages/common/Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu
+
+MAINTAINER palletOne "contract@pallet.one"
+
+RUN apt-get -y update \
+    && apt-get install -yqq wget \
+       git \
+       gcc 
--- vm/baseimages/platforms/golang/base_img_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#read -p "Enter base image tag:" tag
-
-docker build -t palletone/goimg .
-
-echo "Download the base image: palletone/goimg successfully"
-
-exit 0
