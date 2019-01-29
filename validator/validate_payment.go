b05b5ffcad0099fafd84240b722fc51bd50d86a6
郭立华 <guolihua@163.com>
2019-01-29 11:24:31 +0800

    adjust code format

--- validator/validate_payment.go
+++ validator/validate_payment.go
@@ -27,74 +27,72 @@ import (
 )
 
 //Coinbase可以没有输入，就算有输入也没有Preoutpoint
-func (validate *Validate) validateCoinbase(payment *modules.PaymentPayload) ValidationCode{
-return TxValidationCode_VALID
+func (validate *Validate) validateCoinbase(payment *modules.PaymentPayload) ValidationCode {
+	return TxValidationCode_VALID
 }
-
 
 //验证一个Payment
 //Validate a payment message
 //1. Amount correct
 //2. Asset must be equal
 //3. Unlock correct
-func (validate *Validate) validatePaymentPayload(tx *modules.Transaction,msgIdx int, payment *modules.PaymentPayload, isCoinbase bool) ValidationCode {
+func (validate *Validate) validatePaymentPayload(tx *modules.Transaction, msgIdx int, payment *modules.PaymentPayload, isCoinbase bool) ValidationCode {
 
-if isCoinbase{
-	return validate.validateCoinbase(payment)
-}
-if payment.LockTime>0 {
-	// TODO check locktime
-}
+	if isCoinbase {
+		return validate.validateCoinbase(payment)
+	}
+	if payment.LockTime > 0 {
+		// TODO check locktime
+	}
 
-		if len(payment.Inputs)==0{
+	if len(payment.Inputs) == 0 {
+		return TxValidationCode_INVALID_PAYMMENT_INPUT
+	}
+	invokeReqMsgIdx := tx.GetContractInvokeReqMsgIdx()
+	txForSign := tx
+	if msgIdx < invokeReqMsgIdx {
+		txForSign = tx.GetRequestTx()
+	}
+	totalInput := uint64(0)
+	utxos := []*modules.Utxo{}
+	var asset *modules.Asset
+	for inputIdx, in := range payment.Inputs {
+		// checkout input
+		if in == nil || in.PreviousOutPoint == nil {
+			log.Error("payment input is null.", "payment.input", payment.Inputs)
 			return TxValidationCode_INVALID_PAYMMENT_INPUT
 		}
-		invokeReqMsgIdx:=tx.GetContractInvokeReqMsgIdx()
-		txForSign:=tx
-		if msgIdx<invokeReqMsgIdx{
-			txForSign=tx.GetRequestTx()
+		// 合约创币后同步到mediator的utxo验证不通过,在创币后需要先将创币的utxo同步到所有mediator节点。
+		utxo, err := validate.utxoquery.GetUtxoEntry(in.PreviousOutPoint)
+		if utxo == nil || err != nil {
+			return TxValidationCode_INVALID_OUTPOINT
 		}
-		totalInput:=uint64(0)
-		utxos:=[]*modules.Utxo{}
-		var asset *modules.Asset
-		for inputIdx, in := range payment.Inputs {
-			// checkout input
-			if in == nil || in.PreviousOutPoint == nil {
-				log.Error("payment input is null.", "payment.input", payment.Inputs)
-				return TxValidationCode_INVALID_PAYMMENT_INPUT
-			}
-			// 合约创币后同步到mediator的utxo验证不通过,在创币后需要先将创币的utxo同步到所有mediator节点。
-			utxo, err := validate.utxoquery.GetUtxoEntry(in.PreviousOutPoint)
-			if  utxo == nil || err != nil {
-				return TxValidationCode_INVALID_OUTPOINT
-			}
-			utxos=append(utxos,utxo)
-			if asset==nil{
-				asset=utxo.Asset
-			}else{
-				//input asset must be same
-				if !asset.IsSimilar(utxo.Asset){
-					return TxValidationCode_INVALID_ASSET
-				}
-			}
-			totalInput+=utxo.Amount
-			// check SignatureScript
-			err=tokenengine.ScriptValidate(utxo.PkScript,nil,txForSign,msgIdx,inputIdx)
-			if err!=nil{
-				log.Infof("Unlock script validate fail,tx[%s],MsgIdx[%d],In[%d],unlockScript:%x,utxoScript:%x",
-					tx.Hash().String(),msgIdx,inputIdx,in.SignatureScript,utxo.PkScript)
-				return TxValidationCode_INVALID_PAYMMENT_INPUT
-			}else{
-				log.Debugf("Unlock script validated! tx[%s],%d,%d",tx.Hash().String(),msgIdx,inputIdx)
+		utxos = append(utxos, utxo)
+		if asset == nil {
+			asset = utxo.Asset
+		} else {
+			//input asset must be same
+			if !asset.IsSimilar(utxo.Asset) {
+				return TxValidationCode_INVALID_ASSET
 			}
 		}
-
+		totalInput += utxo.Amount
+		// check SignatureScript
+		err = tokenengine.ScriptValidate(utxo.PkScript, nil, txForSign, msgIdx, inputIdx)
+		if err != nil {
+			log.Infof("Unlock script validate fail,tx[%s],MsgIdx[%d],In[%d],unlockScript:%x,utxoScript:%x",
+				tx.Hash().String(), msgIdx, inputIdx, in.SignatureScript, utxo.PkScript)
+			return TxValidationCode_INVALID_PAYMMENT_INPUT
+		} else {
+			log.Debugf("Unlock script validated! tx[%s],%d,%d", tx.Hash().String(), msgIdx, inputIdx)
+		}
+	}
 
 	if len(payment.Outputs) == 0 {
 		log.Error("payment output is null.", "payment.output", payment.Outputs)
 		return TxValidationCode_INVALID_PAYMMENT_OUTPUT
 	}
-	totalOutput:=uint64(0)
+	totalOutput := uint64(0)
 	//Check payment
 	//rule:
 	//	1. all outputs have same asset
@@ -103,16 +101,16 @@ if payment.LockTime>0 {
 		if !asset0.IsSimilar(out.Asset) {
 			return TxValidationCode_INVALID_ASSET
 		}
-	 totalOutput+=out.Value
-	 if totalOutput<out.Value ||out.Value==0{//big number overflow
-	 	return TxValidationCode_INVALID_AMOUNT
-	 }
+		totalOutput += out.Value
+		if totalOutput < out.Value || out.Value == 0 { //big number overflow
+			return TxValidationCode_INVALID_AMOUNT
+		}
 	}
 	//Input Output asset mustbe same
-	if !asset.IsSimilar(asset0){
+	if !asset.IsSimilar(asset0) {
 		return TxValidationCode_INVALID_ASSET
 	}
-	if totalOutput>totalInput{ //相当于手续费为负数
+	if totalOutput > totalInput { //相当于手续费为负数
 		return TxValidationCode_INVALID_AMOUNT
 	}
 	return TxValidationCode_VALID
