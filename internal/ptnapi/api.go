983c9ad705034c122c60f6a17cbb18a4e0f7354a
wzhyuan <wzhyuan0621@126.com>
2019-03-20 20:21:46 +0800

    adjust format

--- internal/ptnapi/api.go
+++ internal/ptnapi/api.go
@@ -2134,7 +2134,7 @@ func (s *PublicTransactionPoolAPI) CreateRawTransaction(ctx context.Context /*s 
 }
 
 //sign rawtranscation
-func SignRawTransaction(icmd interface{}, pubKeyFn tokenengine.AddressGetPubKey, hashFn tokenengine.AddressGetSign,addr common.Address) (ptnjson.SignRawTransactionResult, error) {
+func SignRawTransaction(icmd interface{}, pubKeyFn tokenengine.AddressGetPubKey, hashFn tokenengine.AddressGetSign, addr common.Address) (ptnjson.SignRawTransactionResult, error) {
 	cmd := icmd.(*ptnjson.SignRawTransactionCmd)
 	serializedTx, err := decodeHexStr(cmd.RawTx)
 	if err != nil {
@@ -2253,9 +2253,9 @@ func SignRawTransaction(icmd interface{}, pubKeyFn tokenengine.AddressGetPubKey,
 				// Resize output array to up to and including requested index.
 				payload.Outputs = payload.Outputs[:1+1]
 				pk_addr, err := tokenengine.GetAddressFromScript(payload.Outputs[k].PkScript)
-			    if err != nil {
-				    return ptnjson.SignRawTransactionResult{}, errors.New("Get addr FromScript is err when signtx")
-			    }
+				if err != nil {
+					return ptnjson.SignRawTransactionResult{}, errors.New("Get addr FromScript is err when signtx")
+				}
 				// All but current output get zeroed out.
 				if pk_addr != addr {
 					payload.Outputs[k].PkScript = nil
@@ -2265,12 +2265,12 @@ func SignRawTransaction(icmd interface{}, pubKeyFn tokenengine.AddressGetPubKey,
 			}
 		}
 
-			for _, txin := range payload.Inputs {
-            fmt.Printf("-----------------2115----%+v\n",txin)
-		    }
-		    for _, txout := range payload.Outputs {
-		        fmt.Printf("-----------------2118----%+v\n",txout)
-		    }
+		for _, txin := range payload.Inputs {
+			fmt.Printf("-----------------2115----%+v\n", txin)
+		}
+		for _, txout := range payload.Outputs {
+			fmt.Printf("-----------------2118----%+v\n", txout)
+		}
 	}
 	// All returned errors (not OOM, which panics) encounted during
 	// bytes.Buffer writes are unexpected.
@@ -2461,7 +2461,7 @@ func (s *PublicTransactionPoolAPI) SignRawTransaction(ctx context.Context, param
 	}
 
 	newsign := ptnjson.NewSignRawTransactionCmd(params, &srawinputs, &keys, ptnjson.String(hashtype))
-	result, _ := SignRawTransaction(newsign, getPubKeyFn, getSignFn,addr)
+	result, _ := SignRawTransaction(newsign, getPubKeyFn, getSignFn, addr)
 
 	fmt.Println(result)
 	return result, nil
