41bb9fcfca630c4f1f9b70ce098f5f839637b825
yqx <elva1087penny@163.com>
2019-04-19 14:24:18 +0800

    add senderToken

---
+++ bdd/testcase/utilFunc/createToken.py
@@ -0,0 +1,173 @@
+#-*- coding=utf-8 -*-
+import requests
+import json
+import random
+import re
+
+class createToken(object):
+    global temp
+
+    def __init__(self):
+        self.domain = 'http://localhost:8545/'
+        self.headers = {'Content-Type':'application/json'}
+
+        #self.nickname="QA002"
+
+    def runTest(self):
+        pass
+
+    def getBalance(self,address):
+        data = {
+            "jsonrpc": "2.0",
+            "method": "ptn_getBalance",
+            "params": [address],
+            "id": 1
+        }
+        data = json.dumps(data)
+        response = requests.post(url=self.domain, data=data, headers=self.headers)
+        result1 = json.loads(response.content)
+        try:
+            result =result1['result']
+        except KeyError,error:
+            print "key " + error.message +" not found.\n"
+        else:
+            print 'Current Balance: ' + str(result) + '\n'
+            return result
+
+    def getTokenIdFromTokens(self,address,preToken):
+        result = self.getBalance(address)
+        tokenId = self.getTokenStarts(preToken,result)
+
+        print result
+        return str(tokenId)
+
+    def geneNickname(self):
+        nickname = "qa"+str(random.randint(100,999))
+        self.nickname = nickname
+        print "TokenId: " +nickname+"\n"
+        return nickname
+
+    def getTokenId(self, nickname, dict):
+        for key in dict.keys():
+            if re.search(r'^'+nickname, key):
+                global temp
+                temp = key
+                print key
+        return temp
+
+    def getTokenStarts(self, nickname, dict):
+        for key in dict.keys():
+            if key.startswith(nickname):
+                global temp,tempvalue
+                temp = key
+                tempvalue = dict[key]
+                print temp,tempvalue
+        return temp,tempvalue
+
+    def ccinvoketxPass1(self,senderAddr,recieverAddr,senderAmount,poundage,contractId,method,evidence,nickname,decimalAccuracy,tokenPoundage):
+        data = {
+                "jsonrpc":"2.0",
+                "method":"ptn_ccinvoketxPass",
+                "params":
+                    [senderAddr,recieverAddr,str(senderAmount),str(poundage),contractId,[method,evidence,nickname,str(decimalAccuracy),str(tokenPoundage),senderAddr]],
+                "id":1
+            }
+        data = json.dumps(data)
+        response = requests.post(url=self.domain, data=data, headers=self.headers)
+        result1 = json.loads(response.content)
+        return result1
+
+
+    def listAccounts(self):
+        data = {
+            "jsonrpc": "2.0",
+            "method": "personal_listAccounts",
+            "params":
+                [],
+            "id": 1
+        }
+        data = json.dumps(data)
+        response = requests.post(url=self.domain, data=data, headers=self.headers)
+        result1 = json.loads(response.content)
+        print result1['result'][0]
+        return result1['result'][0]
+
+    def paramGroups(self,dynamic,number,*params):
+        if len(params) < number:
+            paramsList = []
+            paramsList.append(dynamic)
+            print  paramsList
+            for n in params:
+                # paramsList.append(n)
+                print n
+            paramsList.extend(n)
+            print paramsList
+            return paramsList
+        else:
+            for n in params:
+                # paramsList.append(n)
+                print n
+            return n
+
+    def ccinvoketxPass(self,*params):
+        geneAdd = self.listAccounts()
+        n = self.paramGroups(geneAdd,13,*params)
+        #'''
+        data = {
+                "jsonrpc":"2.0",
+                "method":"ptn_ccinvoketxPass",
+                "params":
+                    n,
+                "id":1
+            }
+        data = json.dumps(data)
+        response = requests.post(url=self.domain, data=data, headers=self.headers)
+        result1 = json.loads(response.content)
+        print result1
+        return result1
+        #'''
+
+    def ccinvoketx_apply(self,senderAddr,recieverAddr,senderAmount,poundage,tokenAmount):
+        print self.nickname
+        data = {
+            "jsonrpc": "2.0",
+            "method": "ptn_ccinvoketx",
+            "params":
+                [senderAddr, recieverAddr, str(senderAmount), str(poundage), "PCGTta3M4t3yXu8uRgkKvaWd2d8DREThG43",
+                 ["supplyToken", self.nickname, str(tokenAmount)]],
+            "id": 1
+        }
+        data = json.dumps(data)
+        response = requests.post(url=self.domain, data=data, headers=self.headers)
+        result1 = json.loads(response.content)
+        try:
+            result = result1['result']
+        except KeyError:
+            print "Request ApplyToken failed.\n" + str(result1)
+        else:
+            print 'ApplyToken Result: ' + str(result) + '\n'
+            return result
+
+    def transferToken(self,tokenId,senderAddr,recieverAddr,senderAmount,poundage,evidence,unlocktime):
+            data = {
+                "jsonrpc": "2.0",
+                "method": "ptn_transferToken",
+                "params": [
+                    tokenId,senderAddr,recieverAddr,senderAmount,poundage,evidence,"1",unlocktime
+                ],
+                "id": 1
+            }
+            data=json.dumps(data)
+            response = requests.post(url=self.domain, data=data, headers=self.headers)
+            result1 = json.loads(response.content)
+            try:
+                result = result1['result']
+            except KeyError:
+                print "Request transferToken failed.\n" + str(result1)
+            else:
+                print 'transferToken Result: ' + str(result) + '\n'
+                return result
+
+if __name__ == '__main__':
+    #pass
+    createToken().getTokenIdFromTokens('P1HhWxfQLMgb5TfE56GASURCuitX2XL397G','QA003')
