1a5831680c31cd78d4f43a978fdd67fdbdcc7acb
Yang Jay <yubing9421@qq.com>
2019-05-05 18:36:02 +0800

    add log

--- contracts/manger/ccapi.go
+++ contracts/manger/ccapi.go
@@ -350,21 +350,6 @@ func Invoke(rwM rwset.TxManager, idag dag.IDag, chainID string, deployId []byte,
 	requstId := common.HexToHash(txid)
 	unit.RequestId = requstId
 	log.Infof("Invoke Ok, ProcessProposal duration=%v,rsp=%v,%s", duration, rsp, unit.Payload)
-	//type ContractInvokeResult struct {
-	//	ContractId   []byte             `json:"contract_id"` // contract id
-	//	RequestId    common.Hash        `json:"request_id"`
-	//	FunctionName string             `json:"function_name"`
-	//	Args         [][]byte           `json:"args"`         // contract arguments list
-	//	ReadSet      []ContractReadSet  `json:"read_set"`     // the set data of read, and value could be any type
-	//	WriteSet     []ContractWriteSet `json:"write_set"`    // the set data of write, and value could be any type
-	//	Payload      []byte             `json:"payload"`      // the contract execution result
-	//	TokenPayOut  []*TokenPayOut     `json:"token_payout"` //从合约地址付出Token
-	//	TokenSupply  []*TokenSupply     `json:"token_supply"` //增发Token请求产生的结果
-	//	TokenDefine  *TokenDefine       `json:"token_define"` //定义新Token
-	//}
-	//TODO
-	//getRT(name)
-	fmt.Println("Invoke result:==========================================================", unit)
 	return unit, nil
 }
 
