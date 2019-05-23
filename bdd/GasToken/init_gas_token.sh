a54e119e2d940472670f74781b711d551fbe37e1
shuizhongmose <602554102@qq.com>
2019-05-23 14:45:38 +0800

    fix

--- bdd/GasToken/init_gas_token.sh
+++ bdd/GasToken/init_gas_token.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
+cd ../../
 ## build gptn
-#cd ../../
-#go build ./cmd/gptn
-#cp ./cmd/gptn/gptn ./bdd/GasToken/node/
-#cd ./bdd/GasToken/node
-#chmod +x gptn
+go build ./cmd/gptn
+cp ./cmd/gptn/gptn ./bdd/GasToken/node/
+cd ./bdd/GasToken/node
+chmod +x gptn
 
 # new genesis
 ./gptn newgenesis "" fasle << EOF
@@ -15,13 +14,32 @@ y
 EOF
 
 # edit genesis json
-python ../pylibs/init_chain.py
+gasToken="WWW"
+jsonFile="ptn-genesis.json"
+if [ -e "$jsonFile" ]; then
+    #file already exist, modify
+    sed -i "s/\"gasToken\": \"PTN\"/\"gasToken\": \"$gasToken\"/g" $jsonFile
+else
+    #file not found, new file
+    echo "no $jsonFile"
+    exit -1
+fi
+
+# edit toml file
+tomlFile="ptn-config.toml"
+if [ -e "$tomlFile" ]; then
+    #file already exist, modify
+    sed -i "s/GasToken = \"PTN\"/GasToken = \"$gasToken\"/g" $tomlFile
+else
+    #file not found, new file
+    echo "no $tomlFile"
+    exit -1
+fi
 
 # gptn init
-./gptn newgenesis "" fasle << EOF
+./gptn init << EOF
 1
 EOF
 
 # start gptn
-cd ../../../cmd/gptn
 nohup ./gptn &
