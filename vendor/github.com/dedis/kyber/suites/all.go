38c6cda2626408c016bc9c651a3cbecb75228efb
Devin <studyzy@gmail.com>
2019-05-27 13:20:04 +0800

    run go mod vendor

--- vendor/github.com/dedis/kyber/suites/all.go
+++
@@ -1,9 +0,0 @@
-package suites
-
-import (
-	"go.dedis.ch/kyber/v3/group/edwards25519"
-)
-
-func init() {
-	register(edwards25519.NewBlakeSHA256Ed25519())
-}
--- vendor/github.com/dedis/kyber/xof/doc.go
+++
@@ -1,3 +0,0 @@
-// Package xof holds implementations and testing code for the various
-// extendable output functions.
-package xof
