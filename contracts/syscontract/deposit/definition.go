fd7ec5f83486848c4661356ced359000ef4a9ef9
zhichunqi <1558763837@qq.com>
2019-06-09 16:54:33 +0800

    Modify time format for deposit contract

--- contracts/syscontract/deposit/definition.go
+++ contracts/syscontract/deposit/definition.go
@@ -15,8 +15,6 @@
 package deposit
 
 import (
-	"time"
-
 	"github.com/palletone/go-palletone/dag/modules"
 )
 
@@ -68,6 +66,13 @@ const (
 	Agree    = "approved"
 	Quitting = "quitting"
 	Quited   = "quited"
+	//  时间格式
+	//  Layout1 = "2006-01-02 15"
+	//  Layout2 = "2006-01-02 15:04"
+	//  Layout3 = "2006-01-02 15:04:05"
+	//  目前使用 time.Now().UTC().Format(Layout) 返回字符串
+	Layout1 = "2006-01-02 15"
+	Layout2 = "2006-01-02 15:04:05"
 )
 
 //申请提保证金
@@ -75,7 +80,7 @@ type Cashback struct {
 	//CashbackAddress string               `json:"cashback_address"` //请求地址
 	CashbackTokens *modules.AmountAsset `json:"cashback_tokens"` //请求数量
 	Role           string               `json:"role"`            //请求角色
-	CashbackTime   int64                `json:"cashback_time"`   //请求时间
+	CashbackTime   string               `json:"cashback_time"`   //请求时间
 }
 
 //申请没收保证金
@@ -85,13 +90,13 @@ type Forfeiture struct {
 	ApplyTokens    *modules.AmountAsset `json:"apply_tokens"`    //没收数量
 	ForfeitureRole string               `json:"forfeiture_role"` //没收角色
 	Extra          string               `json:"extra"`           //备注
-	ApplyTime      int64                `json:"apply_time"`      //请求时间
+	ApplyTime      string               `json:"apply_time"`      //请求时间
 }
 
 //交易的内容
 type PayValue struct {
 	PayTokens *modules.AmountAsset `json:"pay_tokens"` //数量和资产
-	PayTime   int64                `json:"pay_time"`   //发生时间
+	PayTime   string               `json:"pay_time"`   //发生时间
 	//PayExtra  string        `json:"pay_extra"`  //额外内容
 }
 
@@ -107,21 +112,21 @@ type PayValue struct {
 
 type DepositBalance struct {
 	Balance        uint64 `json:"balance"`          //  保证金余额
-	EnterTime      int64  `json:"enter_time"`       //  交保证金的时间
-	LastModifyTime int64  `json:"last_modify_time"` //  计算币龄时间
+	EnterTime      string `json:"enter_time"`       //  交保证金的时间
+	LastModifyTime string `json:"last_modify_time"` //  计算币龄时间
 }
 
 type MediatorDeposit struct {
-	ApplyEnterTime int64  `json:"apply_enter_time"` //  申请加入时间
-	ApplyQuitTime  int64  `json:"apply_quit_time"`  //  申请退出时间
+	ApplyEnterTime string `json:"apply_enter_time"` //  申请加入时间
+	ApplyQuitTime  string `json:"apply_quit_time"`  //  申请退出时间
 	Status         string `json:"status"`           //  申请状态  申请、同意、退出
-	AgreeTime      int64  `json:"agree_time"`       //  基金会同意申请时间'
+	AgreeTime      string `json:"agree_time"`       //  基金会同意申请时间'
 	DepositBalance
 }
 
 func NewMediatorDeposit() *MediatorDeposit {
 	return &MediatorDeposit{
-		ApplyEnterTime: time.Now().Unix(),
+		ApplyEnterTime: TimeStr(),
 		Status:         Quited,
 	}
 }
