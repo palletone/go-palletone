7922bf8cecd4bc72391cd8c96a8dc1c0f25981c9
wzhyuan <wzhyuan0621@126.com>
2019-06-14 14:47:53 +0800

    add sendjsontx for test

--- internal/web3ext/ptnjs.go
+++ internal/web3ext/ptnjs.go
@@ -119,7 +119,12 @@ web3._extend({
 			params: 1,
 			inputFormatter: [null]
 		}),
-
+                new web3._extend.Method({
+			name: 'sendJsonTransaction',
+			call: 'ptn_sendJsonTransaction',
+			params: 1,
+			inputFormatter: [null]
+		}),
         new web3._extend.Method({
 			name: 'getBalance',
 			call: 'ptn_getBalance',
