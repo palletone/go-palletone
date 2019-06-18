b03c6c2f1b9c412416ec3b465f421debffef5c7b
etying <wjy@xchainet.com>
2019-06-18 02:11:40 -0700

    test

--- light/sync.go
+++ light/sync.go
@@ -152,7 +152,6 @@ func (pm *ProtocolManager) synchronise(peer *peer, assetId modules.AssetId) {
 	//	atomic.StoreUint32(&pm.fastSync, 1)
 	//}
 
-
 	header := pm.dag.CurrentHeader(assetId)
 	if header != nil && header.Number.Index > 0 {
 		go pm.BroadcastLightHeader(header)
