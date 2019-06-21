06c16d5ffd1cde4cb4b10efdcab4fd27dd5d551c
shuizhongmose <602554102@qq.com>
2019-06-21 13:52:14 +0800

    no message

--- bdd/upload2Ftp.sh
+++ bdd/upload2Ftp.sh
@@ -4,12 +4,12 @@ HOST=39.105.191.26
 USER=$1
 PASS=$2
 LCD=$3
-RCD=pub
-RNAME=$4
+RCD=$4
+RNAME=$5
 lftp -u $USER,$PASS $HOST << EOF
+cd pub
+mkdir $RCD
 cd $RCD
-#mkdir $RCD
-#cd $RCD
 put $LCD $RNAME
 echo "done put"
 EOF
