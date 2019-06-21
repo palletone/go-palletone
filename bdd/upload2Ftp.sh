5174ed140761b24ff38712c6e6fc89b5a97da963
shuizhongmose <602554102@qq.com>
2019-06-21 17:01:43 +0800

    no message

--- bdd/upload2Ftp.sh
+++ bdd/upload2Ftp.sh
@@ -8,17 +8,4 @@ RCD=$4
 RNAME=$5
 echo "script start at `date "+%Y-%m-%d %H:%M:%S"`"
 lftp -e "cd pub; mkdir $RCD; cd $RCD; put $LCD -o $RNAME;exit" -u $USER,$PASS $HOST
-#lftp -e "cd pub; mkdir $RCD; cd $RCD; put $LCD -o $RNAME" -u $USER,$PASS $HOST << EOF
-#echo "------"
-#cd pub
-#ls
-#echo "1111111"
-#mkdir $RCD
-#cd $RCD
-#ls
-#echo "222222"
-#put $LCD -o $RNAME
-#echo "done put"
-#bye
-#EOF
 echo "script end at `date "+%Y-%m-%d %H:%M:%S"`"
