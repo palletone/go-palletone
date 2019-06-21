df27f0b3d4e3b28dae15b073628f757cb6649aa8
shuizhongmose <602554102@qq.com>
2019-06-21 17:00:31 +0800

    no message

--- bdd/upload2Ftp.sh
+++ bdd/upload2Ftp.sh
@@ -7,7 +7,7 @@ LCD=$3
 RCD=$4
 RNAME=$5
 echo "script start at `date "+%Y-%m-%d %H:%M:%S"`"
-lftp -e "cd pub; mkdir $RCD; cd $RCD; put $LCD -o $RNAMEi;exit" -u $USER,$PASS $HOST
+lftp -e "cd pub; mkdir $RCD; cd $RCD; put $LCD -o $RNAME;exit" -u $USER,$PASS $HOST
 #lftp -e "cd pub; mkdir $RCD; cd $RCD; put $LCD -o $RNAME" -u $USER,$PASS $HOST << EOF
 #echo "------"
 #cd pub
