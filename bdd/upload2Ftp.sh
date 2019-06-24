114f41256519e379a934d8037dc0e1e494bb1a50
Devin <studyzy@gmail.com>
2019-06-24 15:50:14 +0800

    Zip log file and ftp upload.

--- bdd/upload2Ftp.sh
+++ bdd/upload2Ftp.sh
@@ -6,8 +6,6 @@ spawn lftp travis:$ftppwd@47.74.209.46
 expect "lftp"
 send "cd pub\r"
 expect "ok"
-send "mput /home/travis/gopath/src/github.com/palletone/go-palletone/bdd/node/log/all.log\r"  
-expect "transferred"
 send "mirror -R /home/travis/gopath/src/github.com/palletone/go-palletone/bdd/logs\r"  
 expect "transferred"
 send "exit\r"
