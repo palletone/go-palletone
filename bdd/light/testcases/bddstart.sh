5bcdd9032256e90a7020608317d73438ee981b78
etying <wjy@xchainet.com>
2019-06-25 23:45:20 -0700

    modify light bdd

--- bdd/light/testcases/bddstart.sh
+++ bdd/light/testcases/bddstart.sh
@@ -27,21 +27,55 @@ echo $account7
 startProduce=`./gptn --exec 'mediator.startProduce()'  attach node1/palletone/gptn.ipc`
 echo $startProduce
 
-#transfer to light account5
+#full account transferPTN to light account5
 curl -H "Content-Type:application/json" -X POST -d  "{\"jsonrpc\":\"2.0\",\"method\":\"wallet_transferToken\",\"params\":[\"PTN\",$moneyaccount,$account5,\"100\",\"1\",\"1\",\"1\"],\"id\":1}" http://127.0.0.1:8545
 
 sleep 5
+#check light account5 balance in full node
 balancecommand=`curl -H "Content-Type:application/json" -X POST -d "{\"jsonrpc\":\"2.0\",\"method\":\"wallet_getBalance\",\"params\":[$account5],\"id\":1}" http://127.0.0.1:8585`
 balanceinfo=`echo $balancecommand`
 balance=`echo $balanceinfo | sed -n '$p'`
 b1=`echo $balance | jq '.result.PTN'`
 b2=`echo $b1`
-echo $b2
 num="\"100\""
-echo $num
 if [ "$b2" = "$num" ];then 
     echo "============transferToken1 ok num"$b2"============"
 else
     echo "============transferToken1 err num:"$b2"============"
 fi
 
+
+#ptn.syncUTXOByAddr("P19wzjSAfVKRY84pPQMsqJSxeVK7oTYEiXt") in light node5
+syncutxocommand=`./gptn --exec "ptn.syncUTXOByAddr($account5)"  attach node_test5/palletone/gptn5.ipc`
+syncutxoinfo=`echo $syncutxocommand`
+value="\"OK\""
+if [ $syncutxoinfo = $value ];then
+    echo "============syncUTXOByAddr ok============"
+else
+    echo "============syncUTXOByAddr err============"
+fi
+
+#ptn.getBalance in light node5
+balancecommand=`./gptn --exec "ptn.getBalance($account5)"  attach node_test5/palletone/gptn5.ipc`
+balanceinfo=`echo $balancecommand`
+temp=`echo ${balanceinfo:7}`
+length=`echo ${#temp}`
+num=$[$lenght-2]
+t1=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
+if [ $t1 = 100 ];then
+    echo "============getBalance in light ok============"
+else
+    echo "============getBalance in light err============"
+fi
+
+
+:<<!
+balancecommand=`./gptn --exec "ptn.getBalance($account5)"  attach node_test4/palletone/gptn4.ipc`
+balanceinfo=`echo $balancecommand`
+temp=`echo ${balanceinfo:7}`
+length=`echo ${#temp}`
+num=$[$lenght-2]
+t1=`echo ${temp:0:$num}`
+echo "=========consle getBalance==========="
+echo $t1
+!
