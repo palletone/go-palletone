0aec7868ce116f3897ebe0ad036293fdf7d7e8d1
郭立华 <guolihua@163.com>
2019-06-27 10:55:12 +0800

    delete debug log

--- consensus/jury/election.go
+++ consensus/jury/election.go
@@ -423,7 +423,7 @@ func (p *Processor) processElectionSigRequestEvent(evt *ElectionSigRequestEvent)
 	//if !crypto.VerifySignature(pk, hash.Bytes(), sig) {
 	//	log.Debugf("[%s]processElectionSigRequestEvent, VerifySignature fail", shortId(reqId.String()))
 	//}
-	log.Debug("processElectionSigRequestEvent", "reqId", shortId(reqId.String()), "evt", evt, "PubKey", pk, "Signature", sig, "hash", hash)
+	//log.Debug("processElectionSigRequestEvent", "reqId", shortId(reqId.String()), "evt", evt, "PubKey", pk, "Signature", sig, "hash", hash)
 	if e, ok := p.mel[reqId]; ok {
 		e.brded = true //关闭签名广播请求
 		e.sigReqEd = true
