af4fac7bcc888b267a8dc4434a1aa87e3c1ff38d
Devin <studyzy@gmail.com>
2019-07-16 18:10:38 +0800

    Fix rlphash bug

--- common/util/rlphash_test.go
+++ common/util/rlphash_test.go
@@ -3,12 +3,12 @@ package util
 import "testing"
 
 type A struct {
-	Data int
+	Data uint
 }
 
 func TestRlpHash(t *testing.T) {
 	for i := 0; i < 10; i++ {
-		a := &A{Data: i}
+		a := &A{Data: uint(i)}
 		hash := RlpHash(a)
 		t.Logf("Number:%d,Hash:%x,Hash2:%x", i, hash, RlpHash(i))
 	}
