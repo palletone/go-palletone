39b17cd54a94193ba1e9d6211b958e0cbd6a5218
Devin <studyzy@gmail.com>
2019-07-18 22:43:47 +0800

    Update wallet API

--- internal/web3ext/walletjs.go
+++ internal/web3ext/walletjs.go
@@ -64,7 +64,7 @@ const Wallet_JS = `
 	        new web3._extend.Method({
 			name: 'signRawTransaction',
 			call: 'wallet_signRawTransaction',
-			params: 1
+			params: 3
 		}),	
                new web3._extend.Method({
 			name: 'sendRlpTransaction',
