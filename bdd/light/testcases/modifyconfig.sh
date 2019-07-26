7dd92aebd43a5c2c7b690c90a26ae95eccfca8a1
Albert·Gou <ggq89@qq.com>
2019-07-26 10:38:19 +0800

    fix err in bdd light testcases

--- bdd/light/testcases/modifyconfig.sh
+++ bdd/light/testcases/modifyconfig.sh
@@ -156,7 +156,7 @@ function addBootstrapNodes()
     do
 	if [ $acount -ne $index ];then
 	    #echo $acount
-	    nodeinfo=`echo $content | jq ".initialMediatorCandidates[ $[$acount-1] ].node"`
+	    nodeinfo=`echo $content | jq ".initialMediatorCandidates[ $[$acount-1] ].pNode"`
 	    array="$array$nodeinfo,"
         fi
 	let ++acount;
