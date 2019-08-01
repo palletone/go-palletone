d3e44e55f87f85dbcff4472fab6da6ac2bbf5591
Albert·Gou <ggq89@qq.com>
2019-08-01 16:45:34 +0800

    Update migration100beta.go

--- dag/migration/migration100beta.go
+++ dag/migration/migration100beta.go
@@ -111,12 +111,10 @@ func (m *Migration100_101) ExecuteUpgrade() error {
 }
 
 func (m *Migration100_101) upgradeMediatorInfo() error {
-	statedb := storage.NewStateDb(m.statedb)
-	oldMediators := statedb.GetPrefix(constants.MEDIATOR_INFO_PREFIX)
-
-	for key, value := range oldMediators {
+	oldMediatorsIterator := m.statedb.NewIteratorWithPrefix(constants.MEDIATOR_INFO_PREFIX)
+	for oldMediatorsIterator.Next() {
 		oldMediator := &OldMediatorInfo{}
-		err := rlp.DecodeBytes(value, oldMediator)
+		err := rlp.DecodeBytes(oldMediatorsIterator.Value(), oldMediator)
 		if err != nil {
 			log.Debugf(err.Error())
 			return err
@@ -128,7 +126,7 @@ func (m *Migration100_101) upgradeMediatorInfo() error {
 			MediatorInfoExpand: oldMediator.MediatorInfoExpand,
 		}
 
-		err = storage.StoreToRlpBytes(m.statedb, []byte(key), newMediator)
+		err = storage.StoreToRlpBytes(m.statedb, oldMediatorsIterator.Key(), newMediator)
 		if err != nil {
 			log.Debugf(err.Error())
 			return err
