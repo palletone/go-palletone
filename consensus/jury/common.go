bdc23fafd731bbcc94df175b229704a6cc136d43
郭立华 <guolihua@163.com>
2019-08-30 09:52:44 +0800

    fix log err

--- consensus/jury/common.go
+++ consensus/jury/common.go
@@ -862,7 +862,7 @@ func checkContractTxFeeValid(dag iDag, tx *modules.Transaction) bool {
 
 	val := math.Max(float64(fees.Amount), timeFee+sizeFee) == float64(fees.Amount)
 	if !val {
-		log.Errorf("[%s]checkContractTxFeeValid invalid, fee amount[%f]-fees[%f], txSize[%f]-timeout[d]s",
+		log.Errorf("[%s]checkContractTxFeeValid invalid, fee amount[%f]-fees[%f], txSize[%f]-timeout[%d]s",
 			shortId(reqId.String()), float64(fees.Amount), timeFee+sizeFee, txSize.Float64(), timeout)
 	}
 	return val
