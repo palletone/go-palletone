6bee89bb6c14f919c849816936576d289932e3bc
Albert·Gou <ggq89@qq.com>
2019-10-11 18:31:20 +0800

    Update validate_unit.go

--- validator/validate_unit.go
+++ validator/validate_unit.go
@@ -304,7 +304,7 @@ func (validate *Validate) validateHeaderExceptGroupSig(header *modules.Header) V
 		}
 		// 1568197800 2019-09-11 18:30:00 testnet分叉修复后，统一的leveldb
 		// 2019-07-11 12:56:46 849c2cb5c7b3fbd37b2ac5f318716f90613259f2 将洗牌算法的种子由时间戳改成hash
-		// 并在 1.0.1版本中应用测洗牌算法
+		// 并在 1.0.1 版本升级后，在主网和测试网中使用新的调度策略
 		if header.Time > 1568197800 { //之前的mediator schedule可能验证不过。
 			vcode := validate.validateMediatorSchedule(header)
 			if vcode != TxValidationCode_VALID {
