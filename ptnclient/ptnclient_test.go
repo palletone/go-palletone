08ee139bb162d39a68f1b7ddd7e14127239d11c7
wzy <wzy@xchainet.com>
2019-10-21 18:28:18 +0800

    fix ptnclinet bugs

--- ptnclient/ptnclient_test.go
+++ ptnclient/ptnclient_test.go
@@ -49,14 +49,14 @@ func TestSimpleContractCcstop(t *testing.T) {
 }
 
 func TestSimpleContractCcquery(t *testing.T) {
-    input := []string{"a", "b"}
-    client, _:= rpc.Dial("http://127.0.0.1:8485")
+    input := []string{"getTokenInfo", "btc"}
+    client, _:= rpc.Dial("http://123.126.106.82:8545")
 	defer client.Close()
-	addr := "P1PwFUG7ydvC1KhGsbyQzXCR8TEgdvx9Hut"
-	result, err := client.Contract_CcQuery(context.Background(), addr,input)
+	addr := "PCGTta3M4t3yXu8uRgkKvaWd2d8DREThG43"
+	result, err := client.Contract_CcQuery(context.Background(),addr,input,10)
 	if err != nil {
 		t.Error("TestSimpleContractCcquery No Pass")
 	}
-	fmt.Println("TestSimpleContractCcquery",result)
-	t.Error("TestSimpleContractCcquery Pass")
+        fmt.Println(result)
+        t.Log("Pass")
 }
