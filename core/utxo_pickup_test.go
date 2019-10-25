1f8cef15e73996820c4e4980da6f18791182d7b0
wzy <wzy@xchainet.com>
2019-10-25 11:42:50 +0800

    fix UT errs for greedy function test

--- core/utxo_pickup_test.go
+++ core/utxo_pickup_test.go
@@ -21,9 +21,10 @@
 package core
 
 import (
+        "fmt"
 	"github.com/palletone/go-palletone/common"
-	//"github.com/stretchr/testify/assert"
-	//"testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 type Utxo4Test struct {
@@ -36,25 +37,7 @@ type Utxo4Test struct {
 func (u *Utxo4Test) GetAmount() uint64 {
 	return u.Amount
 }
-/*func TestNew_Selectutxo_Greedy(t *testing.T) {
-	//log.NewTestLog()
-
-	utxos := []*Utxo4Test{}
-	for i := 0;i < 600; i++ {
-		utxos = append(utxos, &Utxo4Test{Amount: uint64(1+i), TxId: common.Hash{}, MsgIdx: 0, OutIdx: uint32(i)})
-	}
-	
-	ut := Utxos{}
-	for _, u := range utxos {
-		ut = append(ut, u)
-	}
-	result, change, err := Select_utxo_Greedy(ut, 400)
-	assert.Nil(t, err)
-	assert.Equal(t, len(result), 500)
-	assert.Equal(t, change, uint64(200))
-}*/
-
-/*func TestSelect_utxo_Greedy(t *testing.T) {
+func TestSelect_utxo_Greedy(t *testing.T) {
 	//log.NewTestLog()
 
 	utxos := []*Utxo4Test{}
@@ -69,16 +52,56 @@ func (u *Utxo4Test) GetAmount() uint64 {
 	result, change, err := Select_utxo_Greedy(ut, 4)
 	assert.Nil(t, err)
 	assert.Equal(t, len(result), 4)
-	assert.Equal(t, change, uint64(2))
+	assert.Equal(t, change, uint64(7))
 	result, change, err = Select_utxo_Greedy(ut, 6)
 	assert.Nil(t, err)
 	for _, u := range result {
 		t.Logf("Selected: %+v\n", u)
 	}
-	assert.Equal(t, len(result), 3)
-	assert.Equal(t, change, uint64(0))
+	assert.Equal(t, len(result), 4)
+	assert.Equal(t, change, uint64(5))
 	result, change, err = Select_utxo_Greedy(ut, 12)
 	assert.NotNil(t, err)
 	t.Logf("get error:%s", err)
+}
+func TestNew_Selectutxo_Greedy(t *testing.T) {
+	//log.NewTestLog()
 
-}*/
+	utxos := []*Utxo4Test{}
+	for i := 0;i < 600; i++ {
+             utxos = append(utxos, &Utxo4Test{Amount: uint64(i+1), TxId: common.Hash{}, MsgIdx: 0, OutIdx: uint32(i)})
+	}
+	
+	ut := Utxos{}
+	for _, u := range utxos {
+		ut = append(ut, u)
+	}
+	result, change, err := Select_utxo_Greedy(ut, 400)
+        //fmt.Println("result is ",result)
+        fmt.Println("change is ",change)
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), 500)
+	assert.Equal(t, change, uint64(174850))
+
+        result, change, err = Select_utxo_Greedy(ut, 200000)
+        //fmt.Println("result is ",result)
+        fmt.Println("change is ",change)
+        assert.NotNil(t, err)
+        assert.Equal(t, len(result), 0)
+        assert.Equal(t, change, uint64(0))
+
+        result, change, err = Select_utxo_Greedy(ut, 174850)
+        //fmt.Println("result is ",result)
+        fmt.Println("change is ",change)
+        assert.Nil(t, err)
+        assert.Equal(t, len(result), 500)
+        assert.Equal(t, change, uint64(400))
+     
+        result, change, err = Select_utxo_Greedy(ut, 600)
+        //fmt.Println("result is ",result)
+        fmt.Println("change is ",change)
+        assert.Nil(t, err)
+        assert.Equal(t, len(result), 500)
+        assert.Equal(t, change, uint64(174650))
+}
+
