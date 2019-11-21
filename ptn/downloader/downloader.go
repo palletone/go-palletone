6d7147f2469c0629eb5f9c5f91db7a05e6cbc4e5
etying <wjy@xchainet.com>
2019-11-20 22:28:08 -0800

    modify sync

--- ptn/downloader/downloader.go
+++ ptn/downloader/downloader.go
@@ -1419,9 +1419,11 @@ func (d *Downloader) importBlockResults(results []*fetchResult) error {
 	for i, result := range results {
 		blocks[i] = modules.NewUnitWithHeader(result.Header).WithBody(result.Transactions)
 	}
+	s_index := uint64(0)
+	if d.mode == FastSync {
+		s_index = d.GetFastStableIndex()
+	}
 
-	//s_index := uint64(1)
-	s_index := d.GetFastStableIndex()
 	log.Debug("Inserting downloaded chain", "items", len(results),
 		"index", first.Number.Index, "index", last.Number.Index, "fastnum", s_index)
 	if index, err := d.dag.InsertDag(blocks, d.txpool, s_index >= last.NumberU64()); err != nil && err.Error() != dagerrors.ErrUnitExist.Error() {
@@ -1551,8 +1553,11 @@ func (d *Downloader) commitFastSyncData(results []*fetchResult /*, stateSync *st
 		blocks[i] = modules.NewUnitWithHeader(result.Header).WithBody(result.Transactions)
 	}
 
-	//s_index := uint64(1)
-	s_index := d.GetFastStableIndex()
+	s_index := uint64(0)
+	if d.mode == FastSync {
+		s_index = d.GetFastStableIndex()
+	}
+
 	log.Debug("Inserting fast-sync blocks", "items", len(results),
 		"firstnum", first.Number.Index, "lastnumn", last.Number.Index, "fastnum", s_index,
 	)
@@ -1571,8 +1576,10 @@ func (d *Downloader) commitPivotBlock(result *fetchResult) error {
 
 	units := []*modules.Unit{}
 	units = append(units, block)
-	//s_index := uint64(1)
-	s_index := d.GetFastStableIndex()
+	s_index := uint64(0)
+	if d.mode == FastSync {
+		s_index = d.GetFastStableIndex()
+	}
 	log.Debug("Committing fast sync pivot as new head", "index:", block.UnitHeader.Number.Index, "unit", *block,
 		"fastnum", s_index)
 	if _, err := d.dag.InsertDag(units, d.txpool, s_index >= block.NumberU64()); err != nil && err.Error() != dagerrors.ErrUnitExist.Error() {
