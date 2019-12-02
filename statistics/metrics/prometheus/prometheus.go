82770dc9d35e3cf7c42b452259fdafa3580b3014
etying <wjy@xchainet.com>
2019-12-01 23:53:48 -0800

    add query in prometheus

--- statistics/metrics/prometheus/prometheus.go
+++ statistics/metrics/prometheus/prometheus.go
@@ -17,6 +17,7 @@
 package prometheus
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/palletone/go-palletone/common"
 	"github.com/palletone/go-palletone/common/log"
@@ -25,8 +26,51 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 )
+
+type Datasource struct {
+	ID                int    `json:"id"`
+	OrgID             int    `json:"orgId"`
+	Name              string `json:"name"`
+	Type              string `json:"type"`
+	TypeLogoURL       string `json:"typeLogoUrl"`
+	Access            string `json:"access"`
+	URL               string `json:"url"`
+	Password          string `json:"password"`
+	User              string `json:"user"`
+	Database          string `json:"database"`
+	BasicAuth         bool   `json:"basicAuth"`
+	BasicAuthUser     string `json:"basicAuthUser"`
+	BasicAuthPassword string `json:"basicAuthPassword"`
+	WithCredentials   bool   `json:"withCredentials"`
+	IsDefault         bool   `json:"isDefault"`
+	JSONData          struct {
+		HTTPMethod  string        `json:"httpMethod"`
+		KeepCookies []interface{} `json:"keepCookies"`
+	} `json:"jsonData"`
+	SecureJSONFields struct {
+	} `json:"secureJsonFields"`
+	Version  int  `json:"version"`
+	ReadOnly bool `json:"readOnly"`
+}
+
+type DatasourceResp struct {
+	Datasource Datasource `json:"datasource"`
+	ID         int        `json:"id"`
+	Message    string     `json:"message"`
+	Name       string     `json:"name"`
+}
+
+type QueryResp struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string        `json:"resultType"`
+		Result     []interface{} `json:"result"`
+	} `json:"data"`
+}
 
 // Prometheus contains the dashboard internals.
 type Prometheus struct {
@@ -63,16 +107,46 @@ func (db *Prometheus) Start(server *p2p.Server, corss *p2p.Server) error {
 
 	return nil
 }
+
 func (db *Prometheus) QueryHandler() http.Handler {
 	return promhttp.InstrumentMetricHandler(
-		prometheus.DefaultRegisterer, db.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{}),
+		prometheus.DefaultRegisterer, db.HandlerForQuery(prometheus.DefaultGatherer, promhttp.HandlerOpts{}),
 	)
 }
 
-func (db *Prometheus) HandlerFor(reg prometheus.Gatherer, opts promhttp.HandlerOpts) http.Handler {
+func (db *Prometheus) HandlerForQuery(reg prometheus.Gatherer, opts promhttp.HandlerOpts) http.Handler {
 	h := http.HandlerFunc(func(rsp http.ResponseWriter, req *http.Request) {
-		log.Debug("======HandlerFor======")
-		rsp.WriteHeader(200)
+		queryurl := req.URL.RawQuery
+		log.Debug("======HandlerForQuery======", "query", queryurl)
+		respurl, err := url.ParseQuery(queryurl)
+		if err != nil {
+			log.Error("HandlerForQuery ParseQuery", "err:", err)
+			rsp.WriteHeader(400)
+		} else {
+			rsp.Header().Set("Content-Type", "application/json")
+			rsp.Header().Set("Access-Control-Allow-Origin", "*")
+			rsp.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Origin")
+			rsp.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			rsp.Header().Set("Access-Control-Expose-Headers", "Date")
+
+			resp := QueryResp{}
+			resp.Status = "success"
+			resp.Data.ResultType = "scalar"
+
+			if r1, err := strconv.ParseFloat(respurl.Get("time"), 64); err != nil {
+				rsp.WriteHeader(500)
+			} else {
+				resp.Data.Result = append(resp.Data.Result, r1)
+				resp.Data.Result = append(resp.Data.Result, "2")
+
+				if data, err := json.Marshal(resp); err != nil {
+					rsp.WriteHeader(500)
+				} else {
+					rsp.WriteHeader(200)
+					rsp.Write(data)
+				}
+			}
+		}
 	})
 	return h
 }
