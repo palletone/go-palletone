f68d2c76bb274410b79e27fea669c304e9c430b2
郭立华 <guolihua@163.com>
2020-01-07 13:47:01 +0800

    checkTxAddrValid add jjhAd check

--- consensus/jury/common.go
+++ consensus/jury/common.go
@@ -575,7 +575,8 @@ func (p *Processor) checkTxAddrValid(tx *modules.Transaction) bool {
 		if err != nil {
 			return false
 		}
-		if !bytes.Equal(contract.Creator, reqAddr.Bytes()) {
+		jjhAd := p.dag.GetChainParameters().FoundationAddress
+		if jjhAd != reqAddr.String() || !bytes.Equal(contract.Creator, reqAddr.Bytes()) {
 			log.Debugf("[%s]checkTxAddrValid, addr is not equal, Creator[%v], reqAddr[%v]",
 				shortId(reqId.String()), contract.Creator, reqAddr.Bytes())
 			return false
