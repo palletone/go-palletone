21d5073a3cb7b36df03b26e5bdae7687680cf6c0
郭立华 <guolihua@163.com>
2020-01-16 10:56:04 +0800

    add rw process

--- consensus/jury/service.go
+++ consensus/jury/service.go
@@ -579,6 +579,9 @@ func (p *Processor) CheckContractTxValid(rwM rwset.TxManager, tx *modules.Transa
 	if p.validator.CheckTxIsExist(tx) {
 		return false
 	}
+
+	defer rwM.CloseTxSimulator(dag.ContractChainId, reqId.String())
+
 	if _, v, err := p.validator.ValidateTx(tx, false); v != validator.TxValidationCode_VALID && err != nil {
 		log.Errorf("[%s]CheckContractTxValid checkTxValid fail, err:%s", shortId(reqId.String()), err.Error())
 		return false
@@ -664,6 +667,8 @@ func (p *Processor) ContractTxCheckForValidator(tx *modules.Transaction) bool {
 		log.Debugf("[%s]ContractTxCheckForValidator, already exit rstTx", shortId(reqId.String()))
 		return msgsCompareInvoke(tx.TxMessages(), contractTx.rstTx.TxMessages())
 	}
+	defer rwset.RwM.CloseTxSimulator(dag.ContractChainId, reqId.String())
+
 	msgs, err := runContractCmd(rwset.RwM, p.dag, p.contract, tx, nil, p.errMsgEnable) // long time ...
 	if err != nil {
 		log.Errorf("[%s]ContractTxCheckForValidator, runContractCmd,error:%s", shortId(reqId.String()), err.Error())
