bee76d3dcc5491b8bdd43eb6911d4bdec90a55d7
郭立华 <guolihua@163.com>
2020-02-05 14:43:25 +0800

    fix contractReq bug

--- consensus/jury/contractReq.go
+++ consensus/jury/contractReq.go
@@ -103,17 +103,17 @@ func (p *Processor) ContractInstallReq(from, to common.Address, daoAmount, daoFe
 		if err = p.runContractReq(reqId, nil); err != nil {
 			return common.Hash{}, nil, err
 		}
-		tx, err := p.GenContractSigTransaction(from, "", ctx.rstTx, p.ptn.GetKeyStore())
+		ctx := p.mtx[reqId]
+		ctx.rstTx, err = p.GenContractSigTransaction(from, "", ctx.rstTx, p.ptn.GetKeyStore())
 		if err != nil {
 			return common.Hash{}, nil, err
 		}
-		err = p.dag.SaveTransaction(tx)
+		err = p.dag.SaveTransaction(ctx.rstTx)
 		if err != nil {
 			log.Errorf("[%s]ContractInstallReq, SaveTransaction err:%s", shortId(reqId.String()), err.Error())
 			return common.Hash{}, nil, err
 		}
-		ctx := p.mtx[reqId]
-		ctx.rstTx = tx
+		tx := ctx.rstTx
 		_, tpl, err := getContractTxContractInfo(tx, modules.APP_CONTRACT_TPL)
 		if err != nil || tpl == nil {
 			errMsg := fmt.Sprintf("[%s]ContractInstallReq getContractTxContractInfo fail, tpl Name[%s]", shortId(reqId.String()), tplName)
