216c9af223aade90a6cc717a599d664d56a5a79d
Albert·Gou <ggq89@qq.com>
2020-02-21 18:18:29 +0800

    update mediator_connection_mock

--- consensus/mediatorplugin/mediator_plugin.go
+++ consensus/mediatorplugin/mediator_plugin.go
@@ -261,7 +261,7 @@ func (mp *MediatorPlugin) maybeProduceUnit() (ProductionCondition, map[string]st
 	detail["Hash"] = unitHash.TerminalString()
 	detail["ParentHash"] = newUnit.ParentHash()[0].TerminalString()
 
-	// 3. 对 unit 进行群签名和广播
+	// 3. 对 unit 进行群签名
 	go mp.groupSignUnit(scheduledMediator, unitHash)
 
 	// 4. 异步向区块链网络广播新unit
