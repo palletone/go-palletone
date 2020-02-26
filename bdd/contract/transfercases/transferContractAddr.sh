d0b80b69f827fe2f382ce3fd5a8b58ef3e308988
root <root@localhost.localdomain>
2020-02-26 19:14:22 +0800

    fixed transferContractAddr.sh

--- bdd/contract/transfercases/transferContractAddr.sh
+++ bdd/contract/transfercases/transferContractAddr.sh
@@ -3,8 +3,7 @@
 set timeout 30
 set tokenHolder [lindex $argv 0]
 set contractAddr [lindex $argv 1]
-spawn ../../node/gptn --exec "wallet.transferPTN($tokenHolder,contractAddr,1,1)" attach ../../node/palletone/gptn.ipc
+spawn ../../node/gptn --exec "wallet.transferPTN($tokenHolder,$contractAddr,1,1)" attach ../../node/palletone/gptn.ipc
 expect "Passphrase:"
 send "1\n"
 interact
-#chmod +x transferContractAddr.sh 
