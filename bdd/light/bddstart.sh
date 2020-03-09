6278a70779977de2ac1e4e8ffa4bd58a1c428a1f
etying <wjy@xchainet.com>
2020-03-08 23:26:14 -0700

    syncing master bdd light

--- bdd/light/bddstart.sh
+++ bdd/light/bddstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 logpath="/home/travis/gopath/src/github.com/palletone/go-palletone/bdd/logs/light"
+#logpath="../logs/light"
 
 ./preset.sh
 sleep 40
@@ -38,19 +39,10 @@ curl -H "Content-Type:application/json" -X POST -d  "{\"jsonrpc\":\"2.0\",\"meth
 
 sleep 5
 
-
 #ptn.syncUTXOByAddr("P19wzjSAfVKRY84pPQMsqJSxeVK7oTYEiXt") in light node5
 syncutxocommand=`./gptn --exec "ptn.syncUTXOByAddr($account5)"  attach node_test5/palletone/gptn5.ipc`
-syncutxoinfo=`echo $syncutxocommand`
-value="\"ok\""
-echo $syncutxoinfo
-if [ $syncutxoinfo = $value ];then
-    echo "============syncUTXOByAddr account5 ok============"
-    python  -m robot.run -d $logpath --test success ./light.robot
-else
-    echo "============syncUTXOByAddr account5 err:"$syncutxoinfo
-    python  -m robot.run -d $logpath --test fail ./light.robot
-fi
+syncutxoinfo1=`echo $syncutxocommand`
+echo "syncutxoinfo1 = "$syncutxoinfo1
 
 #ptn.getBalance in light node5
 balancecommand=`./gptn --exec "wallet.getBalance($account5)"  attach node_test5/palletone/gptn5.ipc`
@@ -58,50 +50,24 @@ balanceinfo=`echo $balancecommand`
 temp=`echo ${balanceinfo:7}`
 length=`echo ${#temp}`
 num=$[$lenght-2]
-t1=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
-if [ $t1 = 100 ];then
-    echo "============getBalance account5 ok============"
-    python  -m robot.run -d $logpath --test success ./light.robot
-else
-    echo "============getBalance account5 err:"$t1
-    python  -m robot.run -d $logpath --test fail ./light.robot
-fi
-
-
-
+balance1=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
+echo "balance1 = " $balance1
 # transferPTN in light node5 node6
 curl -H "Content-Type:application/json" -X POST -d  "{\"jsonrpc\":\"2.0\",\"method\":\"wallet_transferToken\",\"params\":[\"PTN\",$account5,$account6,\"80\",\"1\",\"1\",\"1\"],\"id\":1}" http://127.0.0.1:8595
 
 sleep 5
 
 syncutxocommand=`./gptn --exec "ptn.syncUTXOByAddr($account6)"  attach node_test6/palletone/gptn6.ipc`
-syncutxoinfo=`echo $syncutxocommand`
-value="\"ok\""
-if [ $syncutxoinfo = $value ];then
-    echo "============syncUTXOByAddr account6 ok============"
-    python  -m robot.run -d $logpath --test success ./light.robot
-else
-    echo "============syncUTXOByAddr account6 err:"$syncutxoinfo
-    python  -m robot.run -d $logpath --test fail ./light.robot
-fi
-
+syncutxoinfo2=`echo $syncutxocommand`
+echo "syncutxoinfo2 = "$syncutxoinfo2
 
 balancecommand=`./gptn --exec "wallet.getBalance($account6)"  attach node_test6/palletone/gptn6.ipc`
 balanceinfo=`echo $balancecommand`
 temp=`echo ${balanceinfo:7}`
 length=`echo ${#temp}`
 num=$[$lenght-2]
-t1=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
-if [ $t1 = 80 ];then
-    echo "============getBalance account6 ok============"
-    python  -m robot.run -d $logpath --test success ./light.robot
-else
-    echo "============getBalance account6 err:"$t1
-    python  -m robot.run -d $logpath --test fail ./light.robot
-fi
-
-
-
+balance2=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
+echo "balance2 = "$balance2
 
 # transferPTN in light node6 node7
 curl -H "Content-Type:application/json" -X POST -d  "{\"jsonrpc\":\"2.0\",\"method\":\"wallet_transferToken\",\"params\":[\"PTN\",$account6,$account7,\"50\",\"1\",\"1\",\"1\"],\"id\":1}" http://127.0.0.1:8605
@@ -109,29 +75,15 @@ curl -H "Content-Type:application/json" -X POST -d  "{\"jsonrpc\":\"2.0\",\"meth
 sleep 5
 
 syncutxocommand=`./gptn --exec "ptn.syncUTXOByAddr($account7)"  attach node_test7/palletone/gptn7.ipc`
-syncutxoinfo=`echo $syncutxocommand`
-value="\"ok\""
-if [ $syncutxoinfo = $value ];then
-    echo "============syncUTXOByAddr account7 ok============"
-    python  -m robot.run -d $logpath --test success ./light.robot
-else
-    echo "============syncUTXOByAddr account7 err:"$syncutxoinfo
-    python  -m robot.run -d $logpath --test fail ./light.robot
-fi
-
+syncutxoinfo3=`echo $syncutxocommand`
+echo "syncutxoinfo3 = "$syncutxoinfo3
 
 balancecommand=`./gptn --exec "wallet.getBalance($account7)"  attach node_test7/palletone/gptn7.ipc`
 balanceinfo=`echo $balancecommand`
 temp=`echo ${balanceinfo:7}`
 length=`echo ${#temp}`
 num=$[$lenght-2]
-t1=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
-if [ $t1 = 50 ];then
-    echo "============getBalance account7 ok============"
-    python  -m robot.run -d $logpath --test success ./light.robot
-else
-    echo "============getBalance account7 err:"$t1
-    python  -m robot.run -d $logpath --test fail ./light.robot
-fi
+balance3=`echo ${temp:0:$num} | sed 's/ //g' | sed 's/"//g'`
+echo "balance3 = "$balance3
 
-
+python  -m robot.run -d $logpath -v syncutxoinfo1:$syncutxoinfo1 -v balance1:$balance1 -v syncutxoinfo2:$syncutxoinfo2 -v balance2:$balance2 -v syncutxoinfo3:$syncutxoinfo3 -v balance3:$balance3 ./light.robot
