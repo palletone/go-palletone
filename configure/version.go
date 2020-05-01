ddb2c5074957ac954e89366b19cf8dd5be32c0bd
Devin <studyzy@gmail.com>
2020-05-01 10:59:41 +0800

    Update version to 1.0.7-hotfix1

--- configure/version.go
+++ configure/version.go
@@ -1,19 +1,5 @@
-// Copyright 2018 PalletOne
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
+// Copyright 2018-2020 PalletOne
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 package configure
 
@@ -22,11 +8,10 @@ import (
 )
 
 const (
-	VersionMajor = 1      // Major version component of the current release
-	VersionMinor = 0      // Minor version component of the current release
-	VersionPatch = 7      // Patch version component of the current release
-	VersionMeta  = "release" // Version metadata to append to the version string
-
+	VersionMajor = 1         // Major version component of the current release
+	VersionMinor = 0         // Minor version component of the current release
+	VersionPatch = 7         // Patch version component of the current release
+	VersionMeta  = "hotfix1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
