4732dead3ec1fdfebf48b3081c5b259e6bdbd78a
Devin <studyzy@gmail.com>
2020-05-29 22:50:05 +0800

    Release 1.0.8

--- configure/version.go
+++ configure/version.go
@@ -8,10 +8,10 @@ import (
 )
 
 const (
-	VersionMajor = 1       // Major version component of the current release
-	VersionMinor = 0       // Minor version component of the current release
-	VersionPatch = 8       // Patch version component of the current release
-	VersionMeta  = "alpha" // Version metadata to append to the version string
+	VersionMajor = 1         // Major version component of the current release
+	VersionMinor = 0         // Minor version component of the current release
+	VersionPatch = 8         // Patch version component of the current release
+	VersionMeta  = "release" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
