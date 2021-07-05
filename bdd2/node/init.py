2d1b5767d4bb923c2209ea8a06e7269316cf3795
郭立华 <guolihua@163.com>
2021-07-05 14:46:52 +0800

    change to python3

--- bdd2/node/init.py
+++ bdd2/node/init.py
@@ -7,14 +7,14 @@ from time import sleep
 child = pexpect.spawn(command="./gptn newgenesis \"\" false",maxread=3000)
 child.expect("Do you want to create a new account as the holder of the token")
 child.sendline("y")
-print child.after
+print(child.after)
 child.expect("Passphrase:")
 child.sendline("1")
-print child.after
+print(child.after)
 child.expect(["Repeat passphrase:"])
 child.sendline("1")
-print child.before
-print child.after
+print(child.before)
+print(child.after)
 child.expect(pexpect.EOF)
 sleep(2)
 
@@ -35,15 +35,15 @@ child.expect("Passphrase:")
 child.sendline("1")
 child.expect(pexpect.EOF)
 EOFLog = child.before
-print EOFLog
+print(EOFLog)
 
 
 
 '''
 child = pexpect.spawn(command="./gptn --exec 'personal.listAccounts' attach palletone/gptn.ipc")
 child.expect(pexpect.EOF)
-print child.before
-print child.before.split('\"')[1]
+print(child.before)
+print(child.before.split('\"')[1])
 GENEADD = child.before.split('\"')[1]
 child = pexpect.spawn(command="./gptn account new")
 # child.logfile = sys.stdout
